e10b3e38b9996e7c4d41ce6c03240463c2173917
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-03-22 17:58:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (64.63%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* recursive
     int fib(int n) {
         
         if(n==1||n==0)
@@ -9,4 +10,36 @@ public:
         return ans;
         
     }
+}; 
+        top down approach or   recursion +  memoizing 
+ 
+    int dp[100];
+    
+     int fib(int n) {
+         if(n==0||n==1)
+             return n;
+         if(dp[n]!=0)
+             return dp[n];
+         
+         int ans;
+         ans=fib(n-1)+fib(n-2);
+         return ans;
+     }
 };
+
+
+Bottom up or iterative dp 
+*/
+     int fib(int n) {
+         int dp[100]={0};
+         dp[1]=1;
+         int ans;
+         for(int i=2;i<=n;i++)
+         {
+             
+              dp[i]=dp[i-1]+dp[i-2];
+         }
+         return dp[n];
+     }
+};
+     
