d43ae4fe29133b8d11b6d2fb9b7b029abaef0321
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-04-08 15:17:06 +0530

    Time: 200 ms (35.68%), Space: 17.9 MB (31.72%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
 
 bool canPartition(vector<int>& nums) {
     int n=(int)nums.size();
@@ -17,8 +18,10 @@ bool canPartition(vector<int>& nums) {
     for(int i=0;i<=n;i++)
         for(int j=0;j<=s;j++)
         {
-            if(i==0)dp[i][j]=0;
-            if(j==0)dp[i][j]=1;
+            if(i==0)
+            dp[i][j]=0;
+            if(j==0)
+            dp[i][j]=1;
         }
     
     for(int i=1;i<=n;i++){
@@ -31,4 +34,50 @@ bool canPartition(vector<int>& nums) {
     }
     return dp[n][s];
 }
+}; */
+     bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+        }
+        if(sum%2 !=0) 
+        {return false;}
+        
+        sum=sum/2;
+        
+        int subset[n+1][sum+1];
+        for(int i=0;i<=n;i++){
+            subset[i][0]=true;
+        }
+        for(int j=1;j<=sum;j++){
+            subset[0][j]=false;
+        }
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(j< nums[i-1])
+                    subset[i][j] = subset[i-1][j];
+           else
+                subset[i][j] = subset[i][j] or subset[i - 1][j - nums[i-1]];
+            }
+            }
+          
+             for(int i=1;i<=n;i++)
+                 
+                 for(int j=1;j<=sum;j++)
+                 {
+                if(j< nums[i-1])
+                    
+                    subset[i][j] = subset[i-1][j];
+            if (j >= nums[i - 1])
+                subset[i][j] = subset[i-1][j] or subset[i - 1][j - nums[i-1]];
+     
+                
+        }
+        return subset[n][sum];
+
+    }
 };
+
+
+
