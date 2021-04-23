b4d95e5e2a64631eaa6ff98d4f71ef38ae63e9df
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-04-23 11:36:55 +0530

    Time: 24 ms (38.25%), Space: 10.8 MB (49.03%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -17,14 +17,14 @@ public:
       return lcs(text1,text2,n,m);
     }
      
-}; 
+}; */
     
     //MEMOIZATION
     
     int dp[1001][1001];
     
     
-    int lcs(string s1,string s2,int m,int n)
+    int lcs(string &s1,string &s2,int m,int n)
     {
         if(m==0||n==0)
             return 0;
@@ -52,20 +52,27 @@ public:
 
         
      
-  */
+  /*
 
     int longestCommonSubsequence(string text1, string text2) {
-		int n = text1.size(), m= text2.size(); 
-        vector<vector<int>> lookup(n+1, vector<int>(m+1, 0));
-        
-        for(int i=1; i <= n; i++){
-            for(int j=1; j <= m; j++){
-                if(text1[i-1] == text2[j-1])
-                    lookup[i][j] = 1 + lookup[i-1][j-1];
-                else
-                    lookup[i][j] = max(lookup[i-1][j], lookup[i][j-1]);
+        int n=text1.size();
+        int m=text2.size();
+        int t[n+1][m+1];
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0||j==0)
+                    t[i][j]=0;
             }
-        }
-        return lookup[n][m];
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                    t[i][j]=1+t[i-1][j-1];
+                else
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
+        return t[n][m];
     }
 };
+*/
