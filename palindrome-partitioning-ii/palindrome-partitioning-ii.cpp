55e003c4489300793834dd56e58fe719496bf542
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-05-09 01:57:45 +0530

    Time: 64 ms (43.37%), Space: 8.1 MB (69.14%) - LeetHub

---
+++ palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,56 @@
+
+class Solution {
+public:
+    int minCut(string s) {
+        vector<vector<bool>> dp(s.size(),vector<bool>(s.size()));
+        for(int g = 0;g<s.size();g++)
+        {
+            for(int i = 0,j = g;j<dp.size();i++,j++)
+            {
+                if(g==0)
+                {
+                    dp[i][j] = true;
+                }
+                else if(g==1)
+                {
+                    if(s[i] == s[j])
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = false;
+                }
+                else
+                {
+                    if(s[i]==s[j])
+                    {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                    else
+                        dp[i][j] = false;
+                }
+            }
+        }
+        vector<int> strg(s.size());
+        strg[0] = 0;
+        for(int j = 1;j<strg.size();j++)
+        {
+            if(dp[0][j])
+            {
+                strg[j] = 0;
+            }
+            else
+            {
+            int minv = INT_MAX;
+            for(int i = j;i>=1;i--)
+            {
+                if(dp[i][j])
+                {
+                    if(strg[i-1]<minv)
+                        minv = strg[i-1];
+                }
+            }
+            strg[j] = minv+1;
+            }
+        }
+        return strg[strg.size()-1];
+    }
+};
