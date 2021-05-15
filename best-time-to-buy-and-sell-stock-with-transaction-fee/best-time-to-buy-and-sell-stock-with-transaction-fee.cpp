b4ad583dda662b2a0471f449d2a56400d5c258d4
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-05-15 22:50:42 +0530

    Time: 100 ms (31.37%), Space: 55.4 MB (29.01%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,9 +1,9 @@
-class Solution {
+/*class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         
         // WHY THIS DOESNT WORK 
-        /*int res=INT_MIN;
+        int res=INT_MIN;
         int total=0;
         for(int i=0;i<prices.size()-1;i++)
         {
@@ -16,7 +16,7 @@ public:
         }
         return res+fee;
     }
-};  */
+};  
          int obsp = -prices[0]; // pehla share ka loan 
             int ossp = 0;
         for(int i=1;i<prices.size();i++)
@@ -38,3 +38,21 @@ public:
                return ossp;
                }
                };
+               
+               
+               */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int sell[prices.size()];
+        int buy[prices.size()];
+        memset(sell,0,sizeof(sell));
+        memset(buy,0,sizeof(buy));
+        buy[0] = -prices[0];
+        for(int i=1; i<prices.size();i++) {
+            buy[i] = max(buy[i-1], sell[i-1] - prices[i]);
+            sell[i] = max(sell[i-1],prices[i] + buy[i-1] - fee);
+        }
+        return max(buy[prices.size()-1],sell[prices.size()-1]);
+    }
+};
