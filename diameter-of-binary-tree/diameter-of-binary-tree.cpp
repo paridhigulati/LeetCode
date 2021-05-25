1f97cf8aa4d63e3483723815fcd3ee977fadc1b9
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-05-26 02:50:41 +0530

    Time: 4 ms (96.09%), Space: 20.2 MB (90.96%) - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public: 
-    
+    /*O(n^2)
+    // we need to know the longer path in left and right sub tree so height for that and then again we visit the nodes to fin the longest path i.e the diameter, hence visiting every node twice 
     int height(TreeNode* &root)
     {
         if(root==NULL)
@@ -27,27 +28,27 @@ public:
        int rh = height(root->right);
       int ld =  diameterOfBinaryTree(root->left);
       int rd = diameterOfBinaryTree(root->right);
-      return max(lh + rh, max(ld,rd));
-    }
-};
-
-/*
-      int depth(TreeNode* root){
-        if(!root) return 0;
-        return 1 + max(depth(root->left), depth(root->right));
-    }
-    void findDiameter(TreeNode* root, int& diameter){
-        if(!root or (!root->left and !root->right)) return;
-        diameter = max(diameter,(depth(root->left)+depth(root->right)));
-        findDiameter(root->left,diameter);
-        findDiameter(root->right,diameter);
-        return;
-    }
-    int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        findDiameter(root, diameter);
-        return diameter;
+      return max(lh + rh  , max(ld,rd));
     }
 };
 
 */
+      int solve(TreeNode* root,int &res){
+        
+        if(root==NULL)
+            return 0;
+        
+        int l = solve(root->left,res);
+        int r = solve(root->right,res);
+        
+        int temp = 1 + max(l,r);
+        int ans = max(temp,l+r+1);
+        res = max(res,ans);
+        return temp;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = INT_MIN;
+        solve(root,res);
+        return res-1;
+    }
+};
