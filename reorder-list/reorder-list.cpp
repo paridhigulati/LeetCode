32637eb5f7f21ac0979e388db922a6c1aa5549e6
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-05-28 09:55:32 +0530

    Time: 32 ms (95.11%), Space: 17.7 MB (94.77%) - LeetHub

--- reorder-list/reorder-list.cpp
+++ reorder-list/reorder-list.cpp
@@ -8,6 +8,16 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+
+/*Find Middle Node in Linked List
+Reverse a Linked List
+Start from the first list and alternatively add elements from both the lists.
+    */
+
+
+/*
 class Solution {
 public:
     
@@ -43,13 +53,13 @@ public:
     
     ListNode* reorderList(ListNode* head)
     {
-        if(head==NULL || head->next==NULL)
+        if(head==NULL || head->next==NULL) //edge case, always check this 
             return head;
         
     ListNode* mid = midL(head);
-    ListNode* newHead = mid->next;
-    mid->next=NULL;
-    newHead = reverse(newHead);
+    ListNode* newHead = mid->next; //handling odd inkedlist case also
+    mid->next=NULL;  // new head becomes tail
+    newHead = reverse(newHead); // reverse 
      ListNode* c1 =head;
      ListNode* c2 = newHead;
          ListNode* f1=NULL;
@@ -72,4 +82,68 @@ public:
         return head;
 }        
 
-};
+}; */
+
+
+//SOLUTION 2 
+class Solution {
+public:
+    
+    ListNode* getMid(ListNode* head)
+    {
+        ListNode *slow,*fast;
+        slow = fast = head;
+        while(fast->next!=NULL&&fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+        
+    }
+    
+    ListNode* getReverse(ListNode* head)
+    {
+        ListNode *cur,*prev;
+        cur=head;
+        prev = NULL;
+        while(cur!=NULL)
+        {
+            ListNode *temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+    
+    void reorderList(ListNode* head) {
+        
+        //  If Linked List contains less than 3 nodes then no need to do anything.
+        if(head==NULL||head->next==NULL||head->next->next==NULL)
+            return;
+        
+        /*  
+            Get Middle Node
+            If there is two middle node then return first Middle.
+        */
+        ListNode* mid = getMid(head);
+        
+        //  Seprate the Second Half of the LinkedList
+        ListNode* secondHalf = mid->next;
+        mid->next=NULL;
+        
+        //  Reverse the Second Half of LinkedList
+        ListNode* revHalf = getReverse(secondHalf);
+        
+        //  Finally Merge in Required Manner
+        while(head!=NULL&&revHalf!=NULL)
+        {
+            ListNode* temp = head->next;
+            head->next=revHalf;
+            revHalf=revHalf->next;
+            head->next->next=temp;
+            head = temp;
+        }
+    }
+}; 
