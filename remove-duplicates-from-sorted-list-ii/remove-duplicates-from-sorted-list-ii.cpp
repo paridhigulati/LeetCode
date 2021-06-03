87ef5141bfb0ce381a692393b46c7a0821950ebc
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-06-03 20:04:14 +0530

    Time: 16 ms (12.54%), Space: 11.2 MB (52.00%) - LeetHub

--- remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -11,27 +11,39 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==nullptr|| head->next==nullptr)
+        // handle edge cases, if list is empty
+        // or if it has one element, return it
+        if (head == nullptr || head->next == nullptr) {
             return head;
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy; //iterator 
-        while(head)
-        {
-        if(head->next&& head->val == head->next->val)
-        {
-            int curr = head->val;
-            while(head && head->val == curr)
-            {
-                head=head->next;
+        }
+        
+        // create an auxiliary head pointer
+        // and a new iterator
+            ListNode* dummy = new ListNode(-1);
+            ListNode* prev = dummy;   
+       
+        while (head ) {
+            
+            if (head->next && head->val == head->next->val) {
+                // if current node value is equal to its next value
+                // skip all nodes of that value
+                int curr = head->val;
+                while (head && head->val == curr) {
+                    head = head->next;
+                }
+            } else {
+                // otherwise, assign node to next iterator
+                // increment new iterator and increment node
+                prev->next = head;
+                prev = prev->next;
+                head = head->next;
             }
         }
-        else {
-        temp->next= head;
-        temp = head;
-        head= head->next;
-        }
-        }
-     temp->next = nullptr;
+        
+        // we need to cut the cord
+        prev->next = nullptr;
+        
+        // return new head
         return dummy->next;
     }
 };
