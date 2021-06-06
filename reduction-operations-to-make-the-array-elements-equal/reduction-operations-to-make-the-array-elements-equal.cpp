badb0e1d8c269b08abeef20cc59bcdce5aa15d39
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-06-06 17:17:35 +0530

    Time: 188 ms (100.00%), Space: 86.7 MB (100.00%) - LeetHub

--- reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -16,7 +16,7 @@ public:/*
         }
         return ans;
     }
-}; */
+}; 
    int reductionOperations(vector<int>& nums)
    {
        sort(nums.begin(), nums.end());
@@ -29,4 +29,26 @@ public:/*
        }
        return count;
    }
+}; 
+    */
+    
+     int reductionOperations(vector<int>& a) {
+        int n=a.size();
+        sort(a.begin(),a.end());
+        vector<int> dp(n);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            if(a[i]==a[i-1]){
+                dp[i]=dp[i-1];
+            }
+            else{
+                dp[i]=dp[i-1]+1;
+            }
+        }
+        int sum=0;
+        for(auto x:dp){
+            sum+=x;
+        }
+        return sum;
+    }
 };
