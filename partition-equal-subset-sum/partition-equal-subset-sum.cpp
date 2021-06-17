527ef220c99dffd37a09c904252ca4c33e51d064
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-06-17 18:57:58 +0530

    Time: 476 ms (8.48%), Space: 20 MB (29.08%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,83 +1,56 @@
+//Time and Space complexity =O(n*sum)
 class Solution {
 public:
-    /*
-
-bool canPartition(vector<int>& nums) {
-    int n=(int)nums.size();
-    int s=0;
-    for(auto k: nums)
-        s+=k;
-    
-    if(s&1)
-        return false;
-    
-    s/=2;
-    int dp[n+1][s+1];
-    memset(dp,0,sizeof(dp));
-           
-    for(int i=0;i<=n;i++)
-        for(int j=0;j<=s;j++)
-        {
-            if(i==0)
-            dp[i][j]=0;
-            if(j==0)
-            dp[i][j]=1;
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0; i<n ; i++){
+            sum += nums[i];
         }
-    
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=s;j++){
-            if(nums[i-1]<=j)
-                dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
-            else
-                dp[i][j]=dp[i-1][j];
-        }
-    }
-    return dp[n][s];
-}
-}; */
-     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum=sum+nums[i];
-        }
-        if(sum%2 !=0) 
-        {return false;}
+        if(sum %2) //if sum is odd
+            return false;
+        sum = sum/2;
+        //dp[i][j] -> if sum 'i' is possible with elements from 0 to j-1
+        //dp[sum+1][n]
+        vector<vector<bool> > dp(sum+1,vector<bool>(n,false));
+        //Sum=0 can be obtained using any number of elements
         
-        sum=sum/2;
+		for(int i=0 ; i<n ; i++)
+            dp[0][i] = true;
+        //Sum>=0 can not be attained without any element
+        for(int i=1 ; i<=sum ; i++)
+            dp[i][0] = false;
         
-        int subset[n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            subset[i][0]=true;
-        }
-        for(int j=1;j<=sum;j++){
-            subset[0][j]=false;
-        }
-         for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                if(j< nums[i-1])
-                    subset[i][j] = subset[i-1][j];
-           else
-                subset[i][j] = subset[i][j] or subset[i - 1][j - nums[i-1]];
+        for(int i=1 ; i<=sum ; i++){
+            for(int j=1 ; j<n ; j++){
+                dp[i][j] = dp[i][j-1];
+                if(i-nums[j-1] >= 0){
+                    dp[i][j] = dp[i][j] || dp[i-nums[j-1]][j-1];
+                }
             }
-            }
-          
-             for(int i=1;i<=n;i++)
-                 
-                 for(int j=1;j<=sum;j++)
-                 {
-                if(j< nums[i-1])
-                    
-                    subset[i][j] = subset[i-1][j];
-            if (j >= nums[i - 1])
-                subset[i][j] = subset[i-1][j] or subset[i - 1][j - nums[i-1]];
-     
-                
         }
-        return subset[n][sum];
-
+        return dp[sum][n-1];
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
