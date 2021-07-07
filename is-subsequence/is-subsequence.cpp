51734027cccecea09351d2d01a2cc5f9689faf2f
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-07-07 18:09:26 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (32.25%) - LeetHub

--- is-subsequence/is-subsequence.cpp
+++ is-subsequence/is-subsequence.cpp
@@ -1,24 +1,40 @@
 class Solution 
 {
 public:
-    bool isSubsequence(string s, string t) 
-    {
-        int dp[s.length()+1][t.length()+1],i,j;
-        for(i=0;i<=s.length();i++)
-        {
-            dp[i][0]=0;
+    /* find LCS between the strings and check if LCS value == length of the first string. 
+
+ bool isSubsequence(string s, string t) {
+      int n=s.size(),m=t.size() ;
+      int dp[n+1][m+1];
+for(int i=0;i<=n;i++)
+{
+for(int j=0;j<=m;j++)
+{
+if(i==0 || j==0)
+dp[i][j]=0;
+else if(s[i-1]==t[j-1])
+dp[i][j]=1+dp[i-1][j-1];
+else
+dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+}
+}
+if(dp[n][m]==n)
+return true;
+else
+return false;
+}
+};
+USING 2 POINTERS 
+    The approach is simple to undertsand. You just have to take two pointers for the two strings. If the character of string s doesn't match with that of string t, you have to increment the pointer of t. If the character matches, increment the pointer for both the strings. At the end, if the pointer reaches the end, return true, else return false.*/
+
+
+    bool isSubsequence(string s, string t) {
+        if(s.size()>t.size())return false;
+        int i=0,j=0;
+        while(i<s.size()&&j<t.size()){
+            if(s[i]==t[j]){i++;j++;}
+            else{j++;}
         }
-        for(i=0;i<=t.length();i++)
-        {
-            dp[0][i]=0;
-        }
-        for(i=1;i<=s.length();i++)
-        {
-            for(j=1;j<=t.length();j++)
-            {
-                dp[i][j]=(s[i-1]==t[j-1]?1+dp[i-1][j-1]:max(dp[i-1][j],dp[i][j-1]));
-            }
-        }
-        return dp[s.length()][t.length()]==s.length();
+        return i==s.size()?true:false;
     }
 };
