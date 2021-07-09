78dd362f0939edb7a8ff517ec1b51f1607f5ba11
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-07-09 15:27:58 +0530

    Time: 288 ms (14.91%), Space: 293.2 MB (12.29%) - LeetHub

--- shortest-common-supersequence/shortest-common-supersequence.cpp
+++ shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,55 +1,99 @@
-class Solution {
-public:
-    string s1,s2;
-    int dp[1005][1005];
-    int lcs(int i,int j)
-    {
-        int ans=0;
-        if(i==s1.length() or j==s2.length())return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])
-            ans=1+lcs(i+1,j+1);
-        else
-            ans=max(lcs(i+1,j), lcs(i,j+1));
-        return dp[i][j]=ans;
+
+    /*string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+        // fill the dp table
+        int dp[m + 1][n + 1];
+        for(int i = 0; i <= m; i++)
+        {
+            for(int j = 0; j <= n; j++)
+            {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if(str1[i - 1] == str2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        string ans;
+        int i = m, j = n;
+        while(i > 0 && j > 0)
+        {
+            // If current character in both the strings are same, then current character is part of shortest supersequence
+            if(str1[i - 1] == str2[j - 1])
+            {
+                ans.push_back(str1[i-1]);
+                i--;
+                j--;
+            }
+            // If current character in X and Y are different & we are moving upwards
+            else if(dp[i - 1][j] > dp[i][j - 1])
+            {
+                ans.push_back(str1[i - 1]);
+                i--;
+            }
+            // If current character in X and Y are different & we are moving leftwards
+            else
+            {
+                ans.push_back(str2[j - 1]);
+                j--;
+            }
+        }
+        // If str2 reaches its end, put remaining characters of str1 in the result string
+        while(i > 0)
+        {
+            ans.push_back(str1[i - 1]);
+            i--;
+        }
+        // If str1 reaches its end, put remaining characters of str2 in the result string
+        while(j > 0)
+        {
+            ans.push_back(str2[j - 1]);
+            j--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
-    string shortestCommonSupersequence(string str1, string str2) 
+}; */
+    class Solution {  //techdose 
+        public:
+    string findLCS(string str1,string str2)
     {
-        s1=str1;
-        s2=str2;
-        memset(dp,-1,sizeof(dp));
-        lcs(0,0);
-        int i=0,j=0;
-        string res;
-        while(i<str1.length() && j<str2.length())
-        {
-            if(str1[i]==str2[j])
+        int len1 = str1.size();
+        int len2 = str2.size();
+        string dp[len1+1][len2+1];
+        
+        for(int i=0;i<=len1;++i)
+            for(int j=0;j<=len2;++j)
             {
-                res+=str1[i];
-                i++;
-                j++;
+                if(i==0 || j==0)
+                    dp[i][j] = "";
+                else if(str1[i-1]==str2[j-1])
+                    dp[i][j] = dp[i-1][j-1]+str1[i-1];
+                else
+                    dp[i][j] = dp[i][j-1].size() > dp[i-1][j].size() ? dp[i][j-1]:dp[i-1][j];
             }
-            else if(dp[i+1][j]<dp[i][j+1])
-            {
-                res+=str2[j];
-                j++;
-            }
-            else 
-            {
-                res+=str1[i];
-                i++;
-            }
-        }
-        while(i<str1.length())
+        return dp[len1][len2];
+    }
+
+    string shortestCommonSupersequence(string str1, string str2) {
+        string ans = "";
+        string lcs = findLCS(str1,str2);
+        
+        int p1=0,p2=0;
+        for(char c: lcs)
         {
-            res+=str1[i];
-            i++;
+            while(str1[p1]!=c)  //Add all non-LCS chars from str1
+                ans += str1[p1++];
+            while(str2[p2]!=c)  //Add all non-LCS chars from str2
+                ans += str2[p2++];
+            
+            ans += c;   //Add LCS-char and increment both ptrs
+            ++p1;
+            ++p2;
         }
-        while(j<str2.length())
-        {
-            res+=str2[j];
-            j++;
-        }
-        return res;
+        ans += str1.substr(p1) + str2.substr(p2);
+        return ans;
     }
 };
