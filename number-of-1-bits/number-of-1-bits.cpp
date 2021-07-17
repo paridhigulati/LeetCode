ca4986488a7f56062ece7094f7f39e0087913e71
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-07-17 14:05:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.40%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-   
-        return __builtin_popcount(n);
+          int count=0;
+   while(n>0)
+   {
+     
+     n =  n&(n-1);
+       count++;
+   }
+        return count;
+      // return __builtin_popcount(n); //counts no. of set bits 
     }
 };
+
+
+// to check if ith bit is set or not 
+//if(nums & (1<<i) != 0 ) 
