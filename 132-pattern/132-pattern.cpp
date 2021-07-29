ab449cad9a5a5daafed157bf1d3c5abcac68695e
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-07-29 13:24:33 +0530

    Time: 64 ms (88.33%), Space: 36.6 MB (99.75%) - LeetHub

--- 132-pattern/132-pattern.cpp
+++ 132-pattern/132-pattern.cpp
@@ -22,33 +22,57 @@ public:
     } 
     }; 
     
-    //NLOGN -->using lower bound */
-
-        int size = nums.size();
-        if (size < 3) {
-            return false;
-        }
-        // Find the biggest element smaller than current (32 pattern) in O(N^2) time
-        vector<int> nextGreatestSmaller(size, INT_MIN);
-        set<int> s;
-        set<int>::iterator it;
-        for (int i = size - 1; i >= 0; i--) {
-            s.insert(nums[i]);
-            it = s.lower_bound(nums[i]);
-            if (it != s.begin()) {
-                it--;
-                nextGreatestSmaller[i] = *it;
-            }
-        }
-        // Find if there exists any 32 pattern on my right
-        int minSoFar = nums[0];
-        for (int i = 0; i < size - 2; i++) {
-            minSoFar = min(minSoFar, nums[i]);
-            if (minSoFar < nextGreatestSmaller[i + 1]) {
-                return true;
-            }
-        }
+  
+//o(n^2)
+    int n = nums.size();   
+int mini = nums[0];
+for(int j=1;j<n-1;j++)
+{
+    for(int k = j+1;k<n;k++)
+    {
+        if(nums[k] < nums[j] and nums[k] > mini)
+        {  return true;}
+       
+    }
+     mini = min(mini, nums[j]);
+}
         return false;
     }
 };
-        
+
+
+
+
+// o (n) using stack 
+        int n = nums.size();
+        int mini[n] ;
+        mini[0]= nums[0];
+        for(int i=1;i<n;i++)
+        mini[i] = min(nums[i], mini[i-1]);
+        stack<int>st;
+        for(int j=n-1;j>=0;j--)
+        {
+            if(nums[j] > mini[j])
+                while(!st.empty() and st.top() <= mini[j])
+                    st.pop();
+            if(!st.empty() and st.top() < nums[j]){
+                return true;
+            st.push(nums[j]);
+        }
+        }
+    return false;
+    }
+}; */
+          
+        int two = INT_MIN;
+        int index = nums.size();
+        for (int i=nums.size()-1; i>=0; i--) {
+            if (nums[i] < two) 
+                return true;
+            while (index < nums.size() && nums[i] > nums[index]) {
+                two = nums[index++];
+            }
+            nums[--index] = nums[i];
+        }
+        return false;
+    } };
