5e38ff021b3c2773e9d3813dd9b0637c09c5fd5b
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-10-08 01:15:57 +0530

    Time: 16 ms (71.17%), Space: 16.2 MB (99.87%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-       int numOfJumps = 0, currentIntervalEnd = 0, farthestReachFoundSoFar = 0;
         
-        for(int i = 0 ; i < nums.size() - 1 ; i++) {     // Because you can stop the work once youve
-                                                                            // reached the last index
-            farthestReachFoundSoFar = max(farthestReachFoundSoFar, i + nums[i]);    // maximize our                                                                                                    
-                                                                                    // reach
-            if(i == currentIntervalEnd) {                // When we reach the end of current intervals
-                                                         // end, we need to make our next jump and
-                numOfJumps++;                            // and update our current interval end with       
-                currentIntervalEnd = farthestReachFoundSoFar;   // the farthest reach found so far
-                                                         // We made sure this was the best possible jump
-            }                                            // because we traversed th whole interval and 
-        }                                                // maximized our reach and now we are sure
-        return numOfJumps;                               // where our next jump will be. Greedy!
-    }  
-    
+        int curr = 0;
+        int jump = 0;
+        int maxReach = 0;
+        
+  int n = nums.size();
+        if(n==1) return 0;
+     for(int i=0;i<n-1;i++){
+     
+         if(nums[i]+i  > curr){
+             curr = nums[i] + i ; 
+          }
+        if(maxReach == i){
+            jump++;
+        maxReach = curr;
+        }
+    }
+        return jump;
+    }
 };
