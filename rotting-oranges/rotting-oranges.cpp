35613e888478e92895aa31e6c721d9ba0329c80f
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-10-18 14:19:32 +0530

    Time: 3 ms (94.01%), Space: 13.1 MB (51.00%) - LeetHub

--- rotting-oranges/rotting-oranges.cpp
+++ rotting-oranges/rotting-oranges.cpp
@@ -1,9 +1,6 @@
-class Solution {
-public:
-      int orangesRotting(vector<vector<int>>& grid) 
-    {
+
         
-        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
+      /*  vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
         
         int m=grid.size();
         int n=grid[0].size();
@@ -47,4 +44,65 @@ public:
         
     }
 };
-   
+   */
+          
+         struct node {
+             int x,y,time;
+             node(int _x, int _y, int _time)
+             {
+                 x = _x;
+                 y = _y;
+                 time = _time;
+             }
+         };
+          class Solution {
+          public:
+     
+           int orangesRotting(vector<vector<int>>& grid) 
+          {
+               int n = grid.size();
+               int m = grid[0].size();
+               int cnt = 0 ;
+               int counter=0;
+               queue<node>q;
+               //push the rotten oranges
+                for(int i=0;i<n;i++)
+                {
+                    for(int j=0;j<m;j++)
+                    {
+                        if(grid[i][j]==2)
+                            q.push(node(i,j,0));
+                        if(grid[i][j]!=0)
+                            cnt++; // counting non empty cells
+                    }
+                }
+                    int ans=0;
+                    while(!q.empty())
+                    {
+                        int x = q.front().x;
+                        int y =q.front().y;
+                        int time= q.front().time;
+                        q.pop();
+                        counter++;
+                        ans = max(ans,time);
+                        // iterate in all directn
+                        int dx[] = { -1, 0, 1,0};
+                        int dy[] = { 0,1,0,-1};
+                        for(int i=0;i<4;i++)
+                        {
+                            int newX = x + dx[i];
+                            int newY = y +dy[i];
+                            if(newX >= 0 and newX <n and newY >=0 and newY < m and grid[newX][newY]==1 ) // fresh orange 
+                            {
+                                grid[newX][newY] = 2; // rot it 
+                                q.push(node(newX , newY, time+1));
+                            }
+                            
+                        }
+                    }
+                    if(counter== cnt) return ans;
+                 return -1;
+                  }
+               
+           };
+    
