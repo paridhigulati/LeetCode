acbe83a78a348adc691b0c9fc39374d510aa531f
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-10-20 01:38:56 +0530

    Time: 44 ms (42.05%), Space: 89.3 MB (60.46%) - LeetHub

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-     // brute -> merge 
+     /* brute -> merge arrays and find median O(m+n) spaces and O(m+n) time
         int n = nums1.size();
         int m = nums2.size();
         vector<int>ans(n+m);
@@ -45,8 +45,48 @@ public:
         
     }
 };
-
-   
+*/
+        // OPTIMAL SOLUTION ->BINARY SEARCH 
+        if(nums2.size() < nums1.size()) 
+            return findMedianSortedArrays(nums2, nums1);
+        int n = nums1.size();
+        int m = nums2.size();
+      
+        int low =0, high =n;
+        while(low<=high)
+        {
+            int cut1 = (low+high)>>1;
+            int cut2 = (n+m+1)/2 - cut1;
+            
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int r1 = cut1 == n ? INT_MAX : nums1[cut1];
+            int r2 = cut2 == m? INT_MAX : nums2[cut2];
+            
+            if(l1 <= r2 and l2 <= r1)
+            {
+                
+                if((n+m)%2)
+                   return max(l1,l2);
+                else
+                   return (max(l1,l2) + min(r1,r2))/2.0;
+            }
+            else if(l1>r2)
+            {
+                high = cut1 -1;
+            }
+            else
+                
+            {
+                low =cut1+1;
+            }
+            
+        }
+        return 0;
+    }
+};
+        
         
     
        
