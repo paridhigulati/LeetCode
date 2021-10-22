f27079940d31701fb33ced53983bf73129b35d4a
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-10-22 14:13:06 +0530

    Time: 112 ms (33.30%), Space: 40 MB (11.85%) - LeetHub

--- 01-matrix/01-matrix.cpp
+++ 01-matrix/01-matrix.cpp
@@ -8,49 +8,55 @@
 //first the source..then level 1 nodes..then level 2 and so on
 //here the case is there are many level 0 nodes
 //so push all of them first!!!
-
 class Solution {
-public:
+    public:
     
-    
-    bool isvalid(int i,int j,int m,int n)
-    {
-        if(i==m||j==n||j<0||i<0)
-            return false;
-        return true;
-    }
-    
-    vector<vector<int>> dir={{1,0},{0,1},{0,-1},{-1,0}};
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) 
-    {
-        queue<pair<int,int>> q;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<vector<int>> dis(m,vector<int>(n,-1));
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size(), n=mat[0].size();
+		
+		//Make a visited array to keep track of visited positions
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+		
+		//Store position at which value is 0, and search by BFS to find nearest 1
+        queue<pair<int, int>> q;
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==0)
-                { 
-                    q.push({i,j});
-                    dis[i][j]=0;
-                }
-            }
+                if(mat[i][j]==0)
+                    q.push({i, j});
+        
+        int level=0;    //To store the nearest distance from position of 0 in queue to nearest 1
         while(!q.empty())
         {
-            pair<int,int> curr=q.front();
-            q.pop();
-            for(auto& x:dir)
+            int size=q.size();
+            while(size--)
             {
-                int a=curr.first+x[0];
-                int b=curr.second+x[1];
-                if(isvalid(a,b,m,n)&&dis[a][b]==-1)
+                int i=q.front().first;
+                int j=q.front().second;
+                q.pop();
+                
+				//If we already visited the node, leave it, else mark it as true
+                if(visited[i][j])
+                    continue;
+                visited[i][j]=true;
+                
+                if(mat[i][j]==1)
+                    mat[i][j]=level;
+                
+				//Traverse in all four directions from the current position
+                int temp[] = {-1,0,1,0,-1};
+                for(int k=0;k<4;k++)
                 {
-                    q.push({a,b});
-                    dis[a][b]=dis[curr.first][curr.second]+1;
+                    int x = i + temp[k];
+                    int y = j + temp[k+1];
+                    
+                    if(x<0 || y<0 || x==m || y==n)
+                        continue;
+                    q.push({x, y});
                 }
             }
+            level++;
         }
-        return dis;
+        return mat;
     }
 };
