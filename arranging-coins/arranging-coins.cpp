344ba0bf14b410a59d7cba7ed48f1d9f033a634d
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-11-05 20:40:15 +0530

    Time: 8 ms (44.64%), Space: 5.8 MB (96.10%) - LeetHub

---
+++ arranging-coins/arranging-coins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int rowCount = 0;
+        int total = 0;
+        while(n>0)
+        {
+            rowCount++;
+            n = n-rowCount;
+        }
+        return n==0? rowCount: rowCount-1;
+    }
+};
