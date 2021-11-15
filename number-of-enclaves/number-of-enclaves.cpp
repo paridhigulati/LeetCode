f95dc93343f68a23acb8c8b998154fdbacc9dbb1
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-11-15 12:43:08 +0530

    Time: 56 ms (66.32%), Space: 21.8 MB (59.96%) - LeetHub

--- number-of-enclaves/number-of-enclaves.cpp
+++ number-of-enclaves/number-of-enclaves.cpp
@@ -1,35 +1,41 @@
 class Solution {
-public:
-          // boundary par jitne bhi connected components h usko hata do, bache hue jo h vo ans 
-     // last row, col and 1
-       
-    //dfs call krke coundary se sare connected components ko 0 krdo aur fir jitne bache hue 1 rhe gye usko count krlo
- 
-     void dfs(int i,int j,vector<vector<int>>& grid){
-        if(i<0 ||i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]==0)
-            return; // boundary wale 
-        grid[i][j]=0; // boundary wale connected components ko zero krdo
-        dfs(i+1,j,grid); 
-        dfs(i,j+1,grid);
-        dfs(i-1,j,grid);
-        dfs(i,j-1,grid);
-    }
-    
-    
-    int numEnclaves(vector<vector<int>>& grid) {
-        int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-       if(i==0 ||j==0 || i==grid.size()-1 || j==grid[0].size()-1) {
-              dfs(i,j,grid);
-                }
-            }
-        }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1) ans++; // grid mai jine bhi 1 bache h vo ans
-            }
-        }
-        return ans;
-    }
-};
+
+ 	int rows, cols;
+ 	void dfs(vector<vector<int>>& A, int i, int j) {
+ 		if (i < 0 || j < 0 || i >= rows || j >= cols)
+ 			return;
+
+ 		if (A[i][j] != 1) 
+ 			return;
+
+ 		A[i][j] = -1;
+ 		dfs(A, i+1, j);
+ 		dfs(A, i-1, j);
+ 		dfs(A, i, j+1);
+ 		dfs(A, i, j-1);
+ 	}
+ public:
+ 	int numEnclaves(vector<vector<int>>& A) {
+
+ 		if (A.empty()) return 0;
+
+ 		rows = A.size();
+ 		cols = A[0].size();
+ 		for (int i = 0; i < rows; i++) {
+ 			for (int j = 0; j < cols; j++) {
+ 				if (i == 0 || j == 0 || i == rows-1 || j == cols-1)
+ 					dfs(A, i, j);
+ 			}
+ 		}
+
+ 		int ans = 0;
+ 		for (int i = 0; i < rows; i++) {
+ 			for (int j = 0; j < cols; j++) {
+ 				if (A[i][j] == 1)
+ 					ans++;
+ 			}
+ 		}
+
+ 		return ans;
+ 	}
+ };
