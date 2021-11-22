407286250734016cd76cd3c97b080c00ebce8861
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-11-23 00:56:07 +0530

    Time: 6 ms (7.07%), Space: 6.2 MB (98.87%) - LeetHub

---
+++ long-pressed-name/long-pressed-name.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+          
+       //
+        if(name.size() > typed.size()) return false;// no possibility 
+        int i =0, j=0; // initialize two pointers to 0
+       
+        while(i< name.size() && j <typed.size()){ // while loop till the size of name and typed string is greater than i and j respectively
+            if(name[i] == typed[j]){ // check if the character at i and j are same if yes than  
+            //increment i & j pointer
+                i++; j++;
+            }else if( i> 0 && name[i-1] == typed[j]){// if the charcters are not same check for the i-1 character & j if they are same than increment j only
+                j++;
+            }else{//if all the above cases are not satisfied return false 
+                return false;
+            }
+        }
+         while(typed.size() > j ){          // alexa alexad
+              if(name.at(i-1) != typed.at(j))
+                  return false;
+               j++;
+        }
+        //return true;
+        return i< name.size() ? false : true;
+    }
+};
