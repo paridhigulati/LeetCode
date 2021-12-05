e3fbdfc8a4f2501c30f2b3fed315ac2aa29a51d6
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-12-05 09:28:38 +0530

    Time: 71 ms (12.22%), Space: 18.7 MB (68.99%) - LeetHub

--- count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
+++ count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) {
-     // brute
+     /* brute
         map<string,int>m1,m2;
      
-        for(auto i : words1)
+        for(auto &i : words1)
         {
             m1[i]++;
         }
-        for(auto i : words2)
+        for(auto &i : words2)
         {
             m2[i]++;
         }
@@ -21,11 +21,29 @@ public:
                     cnt++;
             }
         }
+        return cnt;*/
+        
+        // better -> 1 map 
+        map<string, int>m1;
+        for(auto i : words1)
+         m1[i]++;
+        for(auto i : words2)
+        {
+        if(m1[i] < 2)
+            m1[i]--;
+        }
+        int cnt =0;
+        for(auto i : m1)
+        {
+            if(i.second == 0)
+            {
+                cnt++;
+            }
+        }
+        
+        
+        
         return cnt;
-        
-        
-        
-        
         
         
     }
