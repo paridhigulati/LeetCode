5dc267b1e1face13bb4822ba5ac14b4449aa83a9
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-01-03 13:59:52 +0530

    Time: 200 ms (29.61%), Space: 60.9 MB (56.62%) - LeetHub

--- find-the-town-judge/find-the-town-judge.cpp
+++ find-the-town-judge/find-the-town-judge.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        // should be trusted by n-1 ppl and should trust 0 ppl 
-       
-        vector<pair<int,int>>arr(n+1, {0,0}); // first : how many he trusts 
-        //second : how many trust him 
+        
+        vector<pair<int,int>>arr(n+1, {0,0});
+        //cout<<trust.size();
         for(int i=0;i<trust.size();i++)
         {
-            arr[trust[i][0]].first += 1;
-            arr[trust[i][1]].second += 1;
+            arr[trust[i][0]].first+=1;
+            arr[trust[i][1]].second+=1;
         }
         for(int i=1;i<=n;i++)
-        
-            if(arr[i].first==0 and arr[i].second==n-1)
+        {
+            if(arr[i].first == 0 and arr[i].second==n-1)
                 return i;
-            
+        }
         return -1;
     }
 };
-   
