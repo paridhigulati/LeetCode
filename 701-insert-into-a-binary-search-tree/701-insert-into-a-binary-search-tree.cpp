75c354d005e9a2f4175d8f7c6013627bc8bbce56
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-01-13 01:01:38 +0530

    Time: 118 ms (30.21%), Space: 56.7 MB (99.05%) - LeetHub

---
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+     
+        if(root==NULL)
+    return new TreeNode (val);
+       TreeNode* temp = root;
+        while(true)
+        {
+         if(temp->val > val)
+        {
+             if(temp->left !=NULL)
+             {
+                 temp = temp->left;
+             }
+             else{
+                 temp->left = new TreeNode (val);
+             break;
+         }
+             
+            //go left
+        }
+        else 
+            {
+               if( temp->val <= val)
+        {
+             if(temp->right !=NULL)
+             {
+                 temp = temp->right;
+             }
+             else{
+                 temp->right = new TreeNode (val);
+             break;
+             }
+             
+        }
+        }
+        }
+            //go right 
+      
+        return root;
+    }
+};
