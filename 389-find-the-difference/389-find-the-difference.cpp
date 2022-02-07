59decfc69ffd37636718bac872633d4dce3aceb5
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-02-07 06:36:48 +0530

    Time: 4 ms (67.41%), Space: 7.1 MB (9.15%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
      
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0;i<t.size();i++)
-        {
-            if(t[i]!=s[i])
-             return t[i];
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         for(int i=0;i<t.size();i++)
+//         {
+//             if(t[i]!=s[i])
+//              return t[i];
+//         }
+//         return '-1';
+//     }
+// };
+        string sum= s+t;int ans=0;
+        
+        cout<<s+t;
+        for(int i=0;i<sum.length();i++){
+            ans^=sum[i];
         }
-        return '-1';
+        return ans;
     }
 };
