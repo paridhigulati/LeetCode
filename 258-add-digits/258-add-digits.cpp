c70d63b54178c72711e284e0929b11ac25b1d552
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-02-08 08:18:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,7 +1,12 @@
 
    class Solution {
 public:
+  
+   
     int addDigits(int n) {
-        return n < 10 ? n : addDigits(n / 10 + n % 10);
+        while (n > 9) {
+            n = n / 10 + n % 10;
+        }
+        return n;
     }
 };
