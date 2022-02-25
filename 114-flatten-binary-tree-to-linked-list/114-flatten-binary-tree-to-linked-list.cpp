42aa15c51420fc9f2658512ff72eaa5f0baf0c07
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-02-25 13:32:39 +0530

    Time: 8 ms (62.52%), Space: 12.7 MB (78.55%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,27 +11,40 @@
  */
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        
-
-        if(!root) return;
-        
-        TreeNode* rightNode = root->right;
-       
-        flatten(root->left);
-   
-            //root's immediate right will now point to left Head
-            root->right=root->left;
-            //root'left to null
+    
+        TreeNode* dfs(TreeNode* root) {
+        // edge case: childless node
+        if (!root->left && !root->right) return root;
+            
+        if (root->left and !root->right) {
+            // first case: only the left branch
+           // if (!root->right) {
+                // attaching the flattened left branch directly to the right of root
+                root->right = root->left;
+                // removing the left branch
+                root->left = NULL;
+                // returning the last node of the new right branch (previous left)
+                return dfs(root->right);
+            }
+            else
+            {
+                if(root->left and root->right)
+                {
+            // attaching right to the end of the flatened left branch
+          TreeNode* leftTail = dfs(root->left);
+            leftTail->right = root->right;
+            // attaching the flattened left branch directly to the right of root
+            root->right = root->left;
+            // removing the left branch
             root->left = NULL;
-        while(root->right)
-        {
-           root = root->right; 
-        }
-        flatten(rightNode);
-        root->right = rightNode;
+                }
+            }
+        // if we do not have a left branch, we can just return the flattened right
+        return dfs(root->right);
+    }
+    
+    void flatten(TreeNode* root) {
+        if (root)
+        dfs(root);
     }
 };
-        
-     
-      
