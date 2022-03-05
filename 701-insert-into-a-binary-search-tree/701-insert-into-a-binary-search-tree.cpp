9ea41ff253c21af0b6f73bc7df3e0c67e2896d3a
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-03-05 15:05:05 +0530

    Time: 113 ms (69.74%), Space: 57 MB (50.33%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -11,56 +11,36 @@
  */
 class Solution {
 public:
-    
-    //RECURSIVE -> o(n) tc and sc --- worst case for bst -> skew 
-   
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root == NULL) return new TreeNode(val);
-        if (val > root->val) root->right = insertIntoBST(root->right, val); // Continue searching in the right subtree
-        else root->left = insertIntoBST(root->left, val); // Continue searching in the left subtree
-        return root;
-    }
-};
-/*
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-     
-        if(root==NULL)
-    return new TreeNode (val);
-       TreeNode* temp = root;
+        
+         if(root==NULL)
+            return new TreeNode (val);
+        TreeNode* curr = root;
         while(true)
         {
-         if(temp->val > val)
-        {
-             if(temp->left !=NULL)
-             {
-                 temp = temp->left;
-             }
-             else{
-                 temp->left = new TreeNode (val);
-             break;
-         }
-             
-            //go left
-        }
-        else 
+            if(curr->val >= val)
             {
-               if( temp->val <= val)
-        {
-             if(temp->right !=NULL)
-             {
-                 temp = temp->right;
-             }
-             else{
-                 temp->right = new TreeNode (val);
-             break;
-             }
-             
+                if(curr->left != NULL)
+                    curr = curr ->left;
+                else
+                {
+                    TreeNode* leftNode = new TreeNode(val);
+                      curr->left = leftNode;
+                    break;
+                }
+            }
+             if(curr->val < val)
+            {
+                if(curr->right != NULL)
+                    curr = curr ->right;
+                else
+                {
+                    TreeNode* RightNode = new TreeNode(val);
+                      curr->right= RightNode;
+                    break;
+                }
+            }
         }
-        }
-        }
-            //go right 
-      
         return root;
     }
 };
-*/
