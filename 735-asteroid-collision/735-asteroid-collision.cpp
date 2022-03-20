575c8fc9ec75057dcc2ff720ef64d7e75a23d316
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-03-20 12:33:32 +0530

    Time: 23 ms (38.57%), Space: 17.9 MB (38.70%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        
-        stack<int>st;
-       // st.push(asteroids[0]);
-        for(int i=0;i<asteroids.size();i++)
-        {
-            if(asteroids[i] >0 || st.empty())
-            {
-                st.push(asteroids[i]);
+//     Whenever we encounter a positive value, we will simply push it to the stack.
+// The moment we encounter a negative value, we know some or all or zero positive values will be knocked out of the stack. The negative value may itself be knocked out or it may enter the stack.
+    
+// We will keep on poping the elements from the stack while the asteroids[i] > s.top(). But remember, negative asteroids can never pop another negative asteroids, since they will never meet them. So, the final condition is while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])), we will pop the elements.
+    
+// We have to take care of the case when s.top() == asteroids[i]. In this case one positive element will pop out and negative asteroid won't enter the stack.
+    
+// If after knocking out elements stack becomes empty() or s.top() becomes negative, that means the current asteroids[i] will enter the stack.
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            if(ast[i] > 0 || s.empty()) {
+                s.push(ast[i]);
             }
-            else
-            {
-            while(st.size()>0 and st.top() >0 and abs(asteroids[i]) > st.top()  )
-            {
-                st.pop();
-            }
-                if(st.size()>0 and st.top()== abs(asteroids[i]))
-                {
-                   st.pop();
-                }
-            
             else {
-                    if(st.empty() || st.top() < 0) {
-                        st.push(asteroids[i]);
+                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) {
+                    s.pop();
+                }
+                if(!s.empty() and s.top() == abs(ast[i])) {
+                    s.pop();
+                }
+                else {
+                    if(s.empty() || s.top() < 0) {
+                        s.push(ast[i]);
                     }
-        }
+                }
             }
-       
-    }
-                vector<int>ans(st.size());
-
-        for(int i=st.size()-1;i>=0;i--)
-        {
-            ans[i] = st.top();
-            st.pop();
         }
-        return ans;
+		// finally we are returning the elements which remains in the stack.
+		// we have to return them in reverse order.
+        vector<int> res(s.size());
+        for(int i = (int)s.size() - 1; i >= 0; i--) {
+            res[i] = s.top();
+            s.pop();
+        }
+        return res;
     }
 };
-            
-           
