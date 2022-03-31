a576539a2440cc14485b3c17558e4b691bf39042
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-03-31 11:42:06 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (5.67%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -7,22 +7,34 @@ public:
         
             // if(matrix.size()==0)
             // return false;
-            int low = 0, high = (n*m)-1;
-            while(low <= high)
-            {
-                int mid = low + (high-low)/2;
+//             int low = 0, high = (n*m)-1;
+//             while(low <= high)
+//             {
+//                 int mid = low + (high-low)/2;
                 
-                if(matrix[mid/n][mid%n] == target)
-                    return true;
+//                 if(matrix[mid/n][mid%n] == target)
+//                     return true;
                 
-                else if(matrix[mid/n][mid%n]  > target)
-                    high = mid-1;
-                else 
-                    low = mid+1;
+//                 else if(matrix[mid/n][mid%n]  > target)
+//                     high = mid-1;
+//                 else 
+//                     low = mid+1;
                 
-            }
+//             }
         
+//         return false;
+        
+        //O(M+N) 2pointer
+        int i=0, j= n-1;
+        while(i < m and j>=0)
+        {
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target )
+                j--;
+            else
+                i++;
+        }
         return false;
-        
     }
 };
