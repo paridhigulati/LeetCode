aeef3764abb4613457e27638dcb6ebc38216d9a8
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-04-12 12:10:18 +0530

    Time: 57 ms (24.47%), Space: 26.5 MB (68.12%) - LeetHub

---
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+     
+        // if found someone smaller on right, swap point  aur left par smaller 
+        
+       int mini = INT_MAX, maxi = INT_MIN;
+        
+        int n = nums.size();
+        
+           for(int i=0;i<n-1;i++)
+           {
+            if(nums[i] > nums[i+1] )
+            {
+                mini = min(mini, nums[i+1]);
+          
+            }
+            
+           }
+
+        for(int j=n-1; j>0;j --)
+        
+        {
+            if( nums[j] < nums[j-1])
+                maxi = max(maxi, nums[j-1]);
+        }
+        
+        int l,r;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > mini)
+            {
+                l=i;
+                break;
+            }
+                
+        }
+        for(int j=n-1;j>0;j--)
+        {
+            if(nums[j] < maxi)
+            {
+                r=j;
+                break;
+            }
+        }
+        return r-l>=0 ? r-l+1 : 0;
+    }
+};
