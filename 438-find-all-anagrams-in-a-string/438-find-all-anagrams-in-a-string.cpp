b23baf7448e554604e264d3a4a48edd827948c46
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-04-13 17:25:32 +0530

    Time: 16 ms (69.64%), Space: 8.7 MB (38.00%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-      
-        vector<int> pattern(26,0), string(26,0),res;
-        if(s.size() < p.size())
-            return res;
-        for(int i=0;i<p.size();i++)
+     
+        
+        
+        int i=0,j=0;
+        vector<int>hash(26,0);
+        vector<int>pattern(26,0);
+        int k = p.size();
+        int n = s.size();
+        
+        vector<int>res;
+        
+        for(char ch : p)
         {
-            pattern[p[i]-'a']++;
-            string[s[i]-'a']++;
+            pattern[ch-'a']++;
         }
-        if(pattern== string)
-            res.push_back(0);
-        for(int i = p.size(); i < s.size(); ++i) 
+        
+        while(j < n)
         {
-           string[s[i]-'a']++;           
-            // release and acquire
-            //aage se ek add kro aur peeche see hatao
+          hash[s[j] -'a'] ++;
             
-           // since we added one element to the right, 
-            // one element to the left should be discarded. 
-            //counter for s[i-p.size()] is decremented
-            string[s[i-p.size()]-'a']--;
-           if(pattern == string )  // this comparison takes O(26), i.e O(1), since both vectors are of fixed size 26. Total complexity O(n)*O(1) = O(n)
-               res.push_back(i-p.size()+1); 
+            if(j-i+1 < k)
+                j++;
+            
+            else if(j-i+1 == k)
+            {
+                if(hash == pattern)
+                {
+                   res.push_back(i); 
+                }
+                if(hash[s[i]-'a'] != 0)
+                    hash[s[i]-'a']--;
+                i++;j++;
+                
+                
+                
+            }
         }
+        
+        
         return res;
-           
-        
-        
         
     }
 };
