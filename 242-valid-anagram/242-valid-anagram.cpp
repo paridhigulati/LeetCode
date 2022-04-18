809c4128b25d4fd211f15d29e6176a54a342dd26
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-04-18 23:52:11 +0530

    Time: 15 ms (45.05%), Space: 7.4 MB (8.12%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size() != t.size())
+            return false;
+        unordered_map<int,int>mp;
+        for(char i  : s)
+        {
+            mp[i]++;
+        }
+        for(char i : t)
+        {
+            mp[i]--;
+       
+        if(mp[i] < 0) return false;}
+        
+        return true;
+    }
+};
