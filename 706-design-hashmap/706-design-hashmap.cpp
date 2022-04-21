5f3487b023628cfc9121a2dc40e1ae09f756170f
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-04-21 17:26:15 +0530

    Time: 189 ms (53.05%), Space: 52.1 MB (86.33%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,69 @@
+class MyHashMap {
+    
+    private : vector<vector<pair<int,int>>>v;
+    const int size = 1000;
+public:
+    MyHashMap() {
+        
+     v.resize(size);
+    }
+    
+    void put(int key, int value) {
+        
+        // pehle se exist , ++ else add
+        if(v[key%size].size()==0)
+        {
+            v[key%size].push_back({key,value});
+        }
+           else
+        {
+            bool flag=1;
+            for(int i=0;i<v[key%size].size();i++)
+            {
+                if(v[key%size][i].first==key)
+                  {  
+                    v[key%size][i].second=value;
+                    flag=0;
+                  }
+            }
+            if(flag)
+            {
+                 v[key%size].push_back({key,value});
+            }
+            
+        }
+        
+    }
+        
+    
+    int get(int key) {
+     
+        // agar exist krta h to return value else  -1 
+       for(int i=0;i<v[key%size].size();i++)
+            {
+                if(v[key%size][i].first==key)
+                  {  
+                     return v[key%size][i].second; 
+                  }
+            }
+        return -1;
+    }
+    void remove(int key) {
+         
+          for(int i=0;i<v[key%size].size();i++)
+            {
+                if(v[key%size][i].first==key)
+                  {  
+                    v[key%size].erase(v[key%size].begin()+i); 
+        }
+    }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
