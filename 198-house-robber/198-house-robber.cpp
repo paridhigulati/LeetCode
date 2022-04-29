6dc376c33a9d9c8b5733fa9a5c1ab842aa6f9e68
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-04-30 01:29:00 +0530

    Time: 3 ms (49.98%), Space: 7.8 MB (26.01%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -6,7 +6,7 @@ public:
         if(n==0) return 0;
         if(n==1)
             return nums[0];
-        if(n==2) return max(nums[0], nums[1]);
+       
         
         vector<int>dp(n+1);
        
