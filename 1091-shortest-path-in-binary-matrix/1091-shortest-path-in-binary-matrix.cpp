f6b4fef6f993223fbdc01c1cfe89615ef7aca997
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-16 11:19:33 +0530

    Time: 390 ms (13.80%), Space: 74.7 MB (14.79%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -8,40 +8,35 @@ public:
     }
     int bfs(vector<vector<int>>& grid, int n)
     {
-        // if starting or ending cell is blocked then return -1
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
-            return -1;
+      if(grid[0][0] == 1 || grid[n-1][n-1]==1)return -1;
+        
+        queue<pair<int,int>>q;
+        q.push(make_pair(0,0));
+        vector<vector<int>>dir = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
 
-        queue<pair<int, int>> q;
-        q.push(make_pair(0,0)); // insert the starting cell
-        vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
-        grid[0][0] = 1; // initially  1 step (answer includes the starting cell)
-
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-
-            // if destination already reached then return curr grid cell value
-            if( row == n -1 && col == n -1)
-                return grid[row][col];
-
-            // iterate for all 8 directions
-            for(auto direction : directions){
-                int nrow = row + direction[0];
-                int ncol = col + direction[1];
-
-                // check for valid direction and no obstacle condition
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && grid[nrow][ncol] == 0){
-                    q.push(make_pair(nrow,ncol));
-                    grid[nrow][ncol] = grid[row][col] + 1; // increase value for that cell
-                }
-            }
-
-            // remove the current row,col from queue as it has been processed completely
-            q.pop(); 
+        grid[0][0] = 1;
+        while(!q.empty())
+        {
+        
+        int row = q.front().first;
+        int col = q.front().second;
+        
+        if(row == n-1 and col == n-1) return grid[row][col];
+        
+        for(auto i : dir)
+        {
+            int nRow = row + i[0];
+            int nCol = col + i[1];
+        
+        if(nRow >= 0 and nRow <n and nCol >=0 and nCol < n and grid[nRow][nCol]==0)
+        {
+            q.push(make_pair(nRow, nCol));
+            grid[nRow][nCol] = grid[row][col] + 1;
         }
-
-        // if destination not reached
-        return -1;
-    }
+        }
+        
+        q.pop();
+        }
+    return -1;
+}
 };
