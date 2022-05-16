7bffffd6018042e063cf65111e6ac6f74b96e4ef
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-16 13:04:10 +0530

    Time: 207 ms (5.05%), Space: 34.7 MB (5.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<char>>& grid, int i, int j)
+    void bfs(vector<vector<char>>& grid, int i, int j)
     {
-         int m = grid.size();
-        int n = grid[0].size();
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!='1')
-           return;
+        int n = grid.size();
+        int m = grid[0].size();
         
-        grid[i][j]=-1;
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-         dfs(grid,i,j+1);
-
-    }
-    int numIslands(vector<vector<char>>& grid) {
-     int m = grid.size();
-        int n = grid[0].size();
-        int count = 0;
-        for(int i =0;i<m;i++)
+         queue<pair<int,int>>q;
+        
+         q.push(make_pair(i,j));
+        
+        grid[i][j]= '0'; 
+    
+        vector<vector<int>>dir = { {0,1}, {1,0}, {0,-1}, {-1,0}};
+        
+        while(!q.empty())
         {
-            for(int j=0;j<n;j++)
+            int row = q.front().first;
+            int col = q.front().second;
+           // q.pop();
+            for(auto itr : dir)
             {
-                if(grid[i][j]== '1')
+                int nrow = row + itr[0];
+                int ncol = col + itr[1];
+                
+                if(nrow >= 0 and nrow < n and ncol >= 0 and ncol <m and grid[nrow][ncol] =='1')
                 {
-                 dfs(grid,i,j);
-                  count++;//no. of connected components 
+                    grid[nrow][ncol] = '0';
+                    q.push((make_pair(nrow, ncol)));
+                    
+                }
+            }
+            q.pop();
+        }
+ 
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+       
+        int r=grid.size();
+        int c = grid[0].size();
+        int ans=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == '1'){
+                     bfs(grid,i,j);
+                    ans++;
+                   
                 }
             }
         }
-        return count;
+        return ans;
     }
+    
+    
+    
 };
