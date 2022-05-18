3608f5305a1b3bf99aca80cc380158952dbffba3
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-18 18:35:12 +0530

    Time: 113 ms (5.17%), Space: 34.2 MB (5.05%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int ans = 0;
+    
+    void bfs(int i, int j, vector<vector<int>>& grid)
+    {
+          int n = grid.size();
+        int m = grid[0].size();
+        
+         queue<pair<int,int>>q;
+        
+         q.push(make_pair(i,j));
+        
+        grid[i][j]= 0; 
+    
+        vector<vector<int>>dir = { {0,1}, {1,0}, {0,-1}, {-1,0}};
+        int count=0;
+        
+        while(!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+              count++;
+            q.pop();
+            for(auto itr : dir)
+            {
+                int nrow = row + itr[0];
+                int ncol = col + itr[1];
+                
+                if(nrow >= 0 and nrow < n and ncol >= 0 and ncol <m and grid[nrow][ncol] ==1)
+                {
+                    grid[nrow][ncol] = 0;
+                    
+                    q.push((make_pair(nrow, ncol)));
+                  
+                    
+                }
+            }
+        }
+        
+        ans = max(ans,count);
+ 
+    }   
+        
+    
+   
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+     
+         
+        int r=grid.size();
+        int c = grid[0].size();
+      
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 1){
+                     bfs(i,j,grid);
+                  
+                }
+            }
+        }
+        return  ans;
+    }
+};
