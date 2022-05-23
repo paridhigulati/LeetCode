ffa650edb1cdcb527a2bbc6f3aebe91c8cc66646
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-23 16:51:59 +0530

    Time: 9 ms (23.74%), Space: 10.9 MB (99.95%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,64 +2,50 @@ class Solution {
 public:
     
  
-int findPivot(vector<int>& arr, int n) {
 
-    int s = 0;
-    int e = n-1;
-    int mid = s + (e-s)/2;
 
-    while(s<e) {
 
-        if(arr[mid] >= arr[0])
-        {
-            s = mid+1;
-        }
-        else{
-            e = mid;
-        }
-        mid = s + (e-s)/2;
-    }
-    return s;
-}
-
-       
-    int binarySearch(vector<int>& nums, int target, int s, int e)
-    {
-        int start = s, end = e;
-        while(start <=  end)
-        {
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-            {
-                end = mid-1;
-            }
-            else{
-                start = mid+1;
-            }
-        }
-        return -1;
-    }
     
     int search(vector<int>& nums, int target) {
     
         //BRUTE -> LINEAR SEARCH 
         //OPTIMAL -> find pivot and check if target lies btwn it or not
-        
-        
-        int n = nums.size();
-        
-        int pivot = findPivot(nums, n);
-        if(nums[pivot] <= target and target <= nums[n-1])
-            //bs on second line
-            return binarySearch(nums, target, pivot, n-1);
-        else
-        //bs  on 1st line
-            return binarySearch(nums, target, 0, pivot-1);
-        
-        //return -1;
+   int low =0, high = nums.size()-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(target == nums[mid])
+            {
+                return mid;
+            }
+            else if(nums[mid] >=  nums[low] )
+            {
+                //low to mid part is sorted so check if target lies here 
+                if(target >= nums[low] and target < nums[mid])
+                {
+                    high = mid-1;
+                }
+            
+            else {
+                low = mid+1;
+            }
+          }
+            else if(nums[mid] < nums[high])
+            {
+                //mid to high part is sortedd
+                if(target > nums[mid] and target <= nums[high])
+                {
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+            // else
+            //     low++;
+        }
+                return -1;
     }
+    
 };
     
