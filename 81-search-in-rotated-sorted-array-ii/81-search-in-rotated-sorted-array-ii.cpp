59a1c3cf9e1d4e0beba51a8d8766e8c31ea8819a
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-23 16:46:28 +0530

    Time: 7 ms (73.69%), Space: 14 MB (31.10%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool search(vector<int>& arr, int target) {
+     // allows duplicate elements
+        int low=0, high=arr.size()-1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            
+               if(arr[mid] == target)
+                return true;
+            
+            else if(arr[mid] > arr[low]) //left part
+            {
+                if(target >= arr[low] and target <= arr[mid])
+                high = mid-1;
+                else
+                    low= mid+1;
+            }
+          
+            else if(arr[mid] < arr[low]) //right part
+            {
+                if(target < arr[mid] || target > arr[high] )
+                    high = mid-1;
+                else
+                low = mid+1;
+        }
+            else
+                low++;
+        }
+        return false;
+    }
+};
