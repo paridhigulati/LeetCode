020656be196c5981db7db14778c2180232a0edbc
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-26 22:25:08 +0530

    Time: 26 ms (5.70%), Space: 14.6 MB (8.46%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,45 +2,37 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
-         int m = image.size();
-        int n = image[0].size();
-        
-        int color = image[sr][sc];
-        
-      if(image[sr][sc] == newColor) return image;
-        
-        vector<vector<int>>dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        
         queue<pair<int,int>>q;
-        q.push(make_pair(sr, sc));
+        int color = image[sr][sc];
+        q.push(make_pair(sr,sc));
+       int n = image.size();
+        int m = image[0].size();
+        
+          if(image[sr][sc] == newColor) return image;
         
         image[sr][sc] = newColor;
-        
+        vector<vector<int>>dir = { {0,1}, {0,-1}, {-1,0}, {1,0}};
         while(!q.empty())
         {
             int row = q.front().first;
             int col = q.front().second;
+             q.pop();
             
-        
-        for(auto i : dir)
-        {
-            int nrow = row + i[0];
-            int ncol = col + i[1];
-            
-            if(nrow >= 0 and nrow < m and ncol >= 0 and ncol < n and image[nrow][ncol] == color)
-            {
-                 image[nrow][ncol] = newColor;
-                q.push(make_pair(nrow, ncol));
+           for(auto it : dir)
+           {
+               int nrow =row + it[0];
+               int ncol = col + it[1];
                
-            }
+               if(nrow >= 0  and nrow < n and ncol >=0 and ncol< m and image[nrow][ncol] ==color)
+               {
+                   image[nrow][ncol] = newColor;
+                   q.push(make_pair(nrow, ncol));
+               }
+           }
         }
-            q.pop();
-    }
+       
         return image;
     }
 };
- 
         
-     
-            
-     
+    
