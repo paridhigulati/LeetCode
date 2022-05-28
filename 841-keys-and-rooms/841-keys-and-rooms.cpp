c0902aaf13673b6a10d0124d591b44273cd37816
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-28 16:42:49 +0530

    Time: 24 ms (10.08%), Space: 10.4 MB (55.48%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-
-        void dfs(vector<vector<int>> &graph, int s, vector<bool> &vis){
-        vis[s] = true;
-        for(int i = 0; i < graph[s].size(); i++){
-            if(!vis[graph[s][i]]){
-                dfs(graph, graph[s][i], vis);
-            }
-        }
-    }
-
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int i, n=rooms.size();
-        vector<bool> vis(n, false);
-        dfs(rooms, 0, vis);
-        for(i = 0; i < n; i++){
-            if(!vis[i]){
-                return false;
-            }
+     
+        int count =0, n = rooms.size();
+        vector<bool>vis(n, false);
+        
+
+            dfs(0, n, rooms, vis);
+                for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
+        {
+        if(!vis[i])
+        
+           return false;
         }
+        
+        
         return true;
+    }
+    
+    void dfs(int node, int n, vector<vector<int>>& rooms, vector<bool>& vis)
+    {
+      vis[node] = true;
+        for(auto it : rooms[node])
+        {
+            if(!vis[it])
+                dfs(it,n, rooms,vis);
+        }
     }
 };
