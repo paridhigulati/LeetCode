f40215b522f79234fb82f648bbdb4df5cab8e997
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-31 13:02:12 +0530

    Time: 56 ms (95.28%), Space: 19 MB (91.51%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-       
-        unordered_set<string>st;
-         if(s.size()<k) return false;
-        for(int i=0;i<s.size()-k+1;i++)
-        {
-            string tmp = (s.substr(i, k));
-            st.insert(tmp);
-           // cout<<tmp<<endl;
-        }
-        //cout<<st.size();
-        if(st.size() == pow(2,k))
-            return true;
-        return false;
-    }
-};
-//    for(int i=0;i<s.size()-k+1;i++)
+       //O(N*K)
+//         unordered_set<string>st;
+//          if(s.size()<k) return false;
+//         for(int i=0;i<s.size()-k+1;i++)
+        
+
 //             st.insert(s.substr(i, k));
         
         
-//         //If set's size is equal to 2 power k,
-//         //then we have all the possible substrings else we return false
+
 //         return st.size()==pow(2, k);
+//         }
+//     };
+        
+        //OPTIMISED : O(N) -- ROLLING HASH 
+        
+        // new hash  = ( old hash << 1 ) & (all ones) | last digit 
+        int n = s.size();
+        int need = 1 << k; //100
+        int allOne = need-1, hashVal = 0;
+        vector<bool>flag(1<<k, false);
+        for(int i=0;i<s.size();i++)
+        {
+            hashVal = (hashVal <<  1) & (allOne) | (s[i]-'0');
+            
+            if(i>=k-1 and !flag[hashVal])
+            {
+                flag[hashVal]= true;
+                 need--;
+                if(need == 0)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+        
