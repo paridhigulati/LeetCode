4661e0403b3a4d9b13ce74eda5d09075144bbf90
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-05-31 11:27:03 +0530

    Time: 628 ms (32.70%), Space: 86.9 MB (22.33%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,12 +4,22 @@ public:
        
         unordered_set<string>st;
          if(s.size()<k) return false;
-      
-           for(int i=0;i<s.size()-k+1;i++)
-            st.insert(s.substr(i, k));
-        
-      
-        return st.size()==pow(2, k);
-       
+        for(int i=0;i<s.size()-k+1;i++)
+        {
+            string tmp = (s.substr(i, k));
+            st.insert(tmp);
+           // cout<<tmp<<endl;
+        }
+        //cout<<st.size();
+        if(st.size() == pow(2,k))
+            return true;
+        return false;
     }
 };
+//    for(int i=0;i<s.size()-k+1;i++)
+//             st.insert(s.substr(i, k));
+        
+        
+//         //If set's size is equal to 2 power k,
+//         //then we have all the possible substrings else we return false
+//         return st.size()==pow(2, k);
