19d37ac878cba92d19ea54bb6ef71b30d49f4344
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-06 16:10:10 +0530

    Time: 184 ms (8.79%), Space: 23.7 MB (19.60%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -1,55 +1,94 @@
 class Solution {
-// public:
+public:
 //     int minDays(int n) {
         //>10^8 suggests a logn solution so we get an idea of bfs .
         // space ~ height of tree ~ logn 
         
+// day 0: [11]
+// day 1: [10]
+// day 2: [5, 9]
+// day 3: [3, 4, 8]
+// day 4: [1, 2, 3, 4, 7]
+// day 5: [0, 1, 2, 3, 6]
 
-//         queue<int>q;
-//         q.push(n);
-//         int cnt=0;
-//         while(!q.empty()){
-//             int sz=q.size();
-//             set<int>s;
-//                 for(int i=0;i<sz;i++)
-//                 {
-//                     int x=q.front();
-//                     q.pop();
-//                     if(x==0)return cnt;
-//                     s.insert(x-1);
-//                     if(x%2==0)
-//                         s.insert(x/2);
-//                     if(x%3==0)
-//                     s.insert(x-2*(x/3));
-//                 }
-//             for(auto x: s)
-//                 q.push(x);
-//                  cnt++;
-//         }
-//         return 0;
-
-//     }
-// };
-private:
-    int inner(int n) {
-        if (dp.find(n) != dp.end()) {
-            return dp[n];
+        int minDays(int n) {
+            
+        queue<int>q;
+        q.push(n);
+        int moves=0;
+        set<int>s;
+        s.insert(n);
+            
+        while(!q.empty()){
+            int n=q.size();
+            while(n--){
+                int k=q.front();
+                q.pop();
+                if(k==0){
+                return moves;
+                }
+                if(s.find(k-1)==s.end()){
+                    q.push(k-1);
+                    s.insert(k-1);
+                }
+                if(k%2==0 && s.find(k/2)==s.end()){
+                    q.push(k/2);
+                    s.insert(k/2);
+                }
+                if(k%3==0 && s.find(k/3)==s.end()){
+                    q.push(k/3);
+                    s.insert(k/3);
+                }
+            }
+            moves++;
+            
         }
-        int result = INT_MAX;
-		// from large partitions to small
-        if (n % 3 == 0) result = min(result, inner(n/3));
-        if (n % 2 == 0) result = min(result, inner(n/2));
-		// we don't want to do this step as this is the most expensive 
-		// we don't need to do this if number was divisible by both
-        if (n % 3 != 0 || n% 2 != 0) result = min(result, inner(n-1));
-        dp[n] = result + 1;
-        return dp[n];
-    }
-public: 
-    unordered_map<int, int> dp;
-    int minDays(int n) {
-        dp[0] = 0;
-        dp[1] = 1;
-        return inner(n);
+        return moves;
+        
     }
 };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+// private:
+//     int inner(int n) {
+//         if (dp.find(n) != dp.end()) {
+//             return dp[n];
+//         }
+//         int result = INT_MAX;
+// 		// from large partitions to small
+//         if (n % 3 == 0) result = min(result, inner(n/3));
+//         if (n % 2 == 0) result = min(result, inner(n/2));
+// 		// we don't want to do this step as this is the most expensive 
+// 		// we don't need to do this if number was divisible by both
+//         if (n % 3 != 0 || n% 2 != 0) result = min(result, inner(n-1));
+//         dp[n] = result + 1;
+//         return dp[n];
+//     }
+// public: 
+//     unordered_map<int, int> dp;
+//     int minDays(int n) {
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         return inner(n);
+//     }
+// };
