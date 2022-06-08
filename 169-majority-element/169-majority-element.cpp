03b0b126be1036a392d2285b3e1fb167d5a545fb
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-08 11:18:50 +0530

    Time: 25 ms (61.02%), Space: 19.6 MB (59.52%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
   
-        unordered_map<int,int>mp;
-        int n = nums.size();
+       //moore voting algo : since the maxElement would be present more than n/2 times throughout the array, therefore there would be at least 2 adjacent occurrence of maxElement in the array.
+        
+        int count=0, maxi = 0;
         
         for(int i=0;i<nums.size();i++)
         {
-            mp[nums[i]]++;
+            if(count ==0)
+            {
+             maxi = nums[i];
+            count=1;
+            }
+            else 
+            {
+            if(maxi == nums[i])
+                count++;
+            else
+                count--;
         }
-        for(auto i : mp)
-        {
-            if(i.second > n/2)
-                return i.first;
         }
-        return -1;
+        return maxi;
     }
 };
