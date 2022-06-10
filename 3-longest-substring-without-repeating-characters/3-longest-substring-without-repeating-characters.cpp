fd11df2e2cea23777a62014676fc5364c0f8f5d7
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-10 11:11:36 +0530

    Time: 20 ms (63.29%), Space: 8.9 MB (40.08%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring(string s) { //largest window with all unique char 
+   
+        if(s.size()==0) return 0;
+        int  n = s.size(), i=0;
+        int j= 0, ans = INT_MIN;
+        unordered_map<int,int>mp;
      
-    if(s.length()==0)return 0;   //if string of length zero comes simply return 0
-        unordered_map<char,int> m;   //create map to store frequency,(get to know all unique characters
-        int i=0,j=0,ans=INT_MIN; 
-        while(j<s.length())   
+        while(j < n)
         {
-            m[s[j]]++;  //increase the frequency of the element as you traverse the string
-            if(m.size()==j-i+1)  // whem map size is equal to the window size means suppose window size is 3 and map size is also three that means in map all unique characters are their
+            mp[s[j]]++;
+        
+     
+            if(mp.size() == j-i+1)
             {
-                ans = max(ans,j-i+1);  //compare the length of the maximum window size
+                ans = max(ans, j-i+1);
             }
-            else if(m.size()<j-i+1)   //if the map size is less than the window size means there is some duplicate present like window size = 3 and map size = 2 means there is a duplicates
-            {
-                while(m.size()<j-i+1)  //so till the duplicates are removed completely
-                {
-                    m[s[i]]--;   //remove the duplicates
-                    if(m[s[i]]==0)  //if the frequency becomes zero 
-                    {
-                        m.erase(s[i]);//delete it completely
-                    }
-                    i++;  //go for next element 
-                }
-            }
-             j++;  //go for the next element
+            
+          else if ( mp.size() < j-i+1)
+          {
+              while(mp.size() < j-i+1)
+              {
+                  mp[s[i]]--;
+                  
+                  if(mp[s[i]] == 0)
+                   mp.erase(s[i]);
+                  
+                  i++;
+              }
+          }
+          
+     j++;
+        
         }
         return ans;
     }
