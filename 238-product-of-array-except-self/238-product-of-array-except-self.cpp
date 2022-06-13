83ce673120f9bd8513f7de4478d42aafd985c7ee
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-13 12:53:11 +0530

    Time: 30 ms (72.29%), Space: 25 MB (36.60%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+   // vector<int> productExceptSelf(vector<int>& nums) {
         //ques to be asked -> does it contain zeroes 
         // brute using division better using 2 array left and right 
         //optimal 
         
-        int  n = nums.size();
-         vector<int>ans(n,1);
+//         int  n = nums.size();
+//          vector<int>ans(n,1);
         
-        
-        for(int i=1;i<n;i++)
-        {
-            ans[i] = ans[i-1] * nums[i-1];
+       
+//         for(int i=1;i<n;i++)
+//         {
+//             ans[i] = ans[i-1] * nums[i-1];
             
-        }
-        
-       int curr = nums[n-1];
-        for(int i= n-2; i>=0;i--)
-        {
-            ans[i] *= curr;
-            curr *= nums[i];
-        }
-        return ans;
-    }
+//         }
+//         int curr = nums[n-1];
+//         for(int i=n-2;i>=0;i--)
+//         {
+//            ans[i] *= curr;
+//             curr *= nums[i];
+//         }
+//         return ans;
+//     }
    
+// };
+          vector<int> productExceptSelf(vector<int>& nums) {
+              int n=nums.size();
+        vector<int> pref(n,1), suf(n,1), ans(n,1);
+              
+              
+              for(int i=1;i<n;i++)
+              {
+                  pref[i] = pref[i-1] * nums[i-1];
+              }
+              for(int i=n-2; i>=0;i--)
+              {
+                  suf[i] = suf[i+1]* nums[i+1];
+              }
+
+           for(int i = 0; i < n; i++)
+            ans[i] = pref[i] * suf[i];
+              
+              return ans;
+    }
 };
