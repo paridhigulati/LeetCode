daaaa347d1b1873e583b70d98f1310f960ae9e8d
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-19 00:02:54 +0530

    Time: 95 ms (75.37%), Space: 14.4 MB (55.92%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,32 +2,21 @@ class Solution {
 public:
       int coinChange(vector<int>& coins, int amount) {
           int n = coins.size();
-        int dp[n+1][amount+1];
-        
-        
-        // dp[i][j] represents the min i coins to make up the amount j
-        for (int i = 0; i <= n; i++) {
-            
-           for(int j=0;j<=amount;j++)
-           {                
-                if (j==0) { // amount = 0
-                    
-                 dp[i][j]= 0; //first col = 0 becoz for 0 amount, 0 coins
-                }
-               else if(i==0) // zero coin but definite sum so INFINITY
-               {
-                   dp[i][j]= 1e5;
-               }
-               else if(coins[i-1]>j) // for coin 3, 1 and 2 have to excluded 
-               {
-                   dp[i][j] = dp[i-1][j];
-               }
-               else
-               {
-                   dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-               }
-           }
-        }
-               return dp[n][amount] > 1e4 ? -1: dp[n][amount];
-           }
-        };
+     
+          vector<int>dp(amount+1, amount+1);
+          
+          dp[0]=0;
+          for(int i=1;i<=amount;i++)
+          {
+              for(int j=0;j<n;j++)
+              {
+                  if( i-coins[j] >=0 )
+                  {
+                      dp[i] = min(dp[i], 1 + dp[i-coins[j]]);
+                  }
+              }
+          }
+          return dp[amount]>amount?-1:dp[amount];
+      }
+};
+    
