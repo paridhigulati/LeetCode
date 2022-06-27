05ccef2f95d0b4daf695f880f425ec8d57b5ab94
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-27 12:49:32 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (63.86%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.cpp
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.cpp
@@ -12,6 +12,17 @@
 class Solution {
 public:
     
+    
+    // Given that total nodes are n.*
+// For any node x,
+// a.find the count of nodes in the left subtree
+// b.and the count of nodes in the right subtree
+// c.Count of nodes above node x(remaining tree) will be n-(a)-(b)-1
+// If player 2 picks the right child, the right subtree is blocked for player 1
+// If he picks the left child, the left subtree is blocked.Similary on picking the parent node, the remaining nodes are blocked
+// Condition for player 2 to win:
+// a>b+c or b>a+c or c>a+b*
+    
     int count(TreeNode* root)
     {
         if(!root) return 0;
@@ -93,44 +104,4 @@ public:
 
 
 
-// Given that total nodes are n.*
-// For any node x,
-// a.find the count of nodes in the left subtree
-// b.and the count of nodes in the right subtree
-// c.Count of nodes above node x(remaining tree) will be n-(a)-(b)-1
-// If player 2 picks the right child, the right subtree is blocked for player 1
-// If he picks the left child, the left subtree is blocked.Similary on picking the parent node, the remaining nodes are blocked
-// Condition for player 2 to win:
-// a>b+c or b>a+c or c>a+b*
-// class Solution {
-// public:
 
-// int count(TreeNode* root)
-// {
-//     if(root==NULL) return 0;
-    
-//     return 1+count(root->left)+count(root->right);
-// }
-
-// TreeNode* find(TreeNode*root,int x)
-// {
-//     if(root==NULL)
-//         return NULL;
-//     if(root->val==x)
-//         return root;
-    
-//     TreeNode* l=find(root->left,x);
-//     TreeNode* r=find(root->right,x);
-//     if(l==NULL) return r;
-//     return l;
-// }
-// bool btreeGameWinningMove(TreeNode* root, int n, int x) {
-//    TreeNode* xpos=find(root,x);
-//    int a=count(xpos->left);
-//    int b=count(xpos->right);
-//    int c=n-a-b-1;
-//    if(a>b+c) return true;
-//    if(b>a+c) return true;
-//    if(c>a+b) return true;
-//    return false;
-// }
