d25cf05a12937b24a0734d5930899b0a2369c861
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-06-27 22:42:21 +0530

    Time: 16 ms (6.58%), Space: 17.4 MB (5.09%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -11,22 +11,29 @@
  */
 class Solution {
 public:
-  void binaryTreePaths(vector<string>& result, TreeNode* root, string t) {
-    if(!root->left && !root->right) {
-        result.push_back(t);
-        return;
+    
+    vector<string> dfs(TreeNode* root, string str, vector<string>& ans)
+    {
+        if(!root) return ans;
+        
+        if(root->left || root->right)  
+            str +=(to_string(root->val) + "->");
+    
+        else
+        { 
+            str += to_string(root->val);
+            ans.push_back(str);
+        }
+        dfs(root->left,str, ans);
+        dfs(root->right,str,ans);
+        
+        return ans;
     }
-
-    if(root->left) binaryTreePaths(result, root->left, t + "->" + to_string(root->left->val));
-    if(root->right) binaryTreePaths(result, root->right, t + "->" + to_string(root->right->val));
-}
-
-vector<string> binaryTreePaths(TreeNode* root) {
-    vector<string> result;
-    if(!root) return result;
-    
-    binaryTreePaths(result, root, to_string(root->val));
-    return result;
-}
-    
+    vector<string> binaryTreePaths(TreeNode* root) {
+          vector<string>ans;
+        string str = "";
+        return dfs(root, str, ans);
+    }
 };
+
+   
