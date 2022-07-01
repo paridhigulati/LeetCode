c1111b08968e1073e08e40927ca79cf7b3ce4567
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-01 19:06:02 +0530

    Time: 103 ms (35.37%), Space: 15.9 MB (77.60%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     
-    bool static comp(vector<int> &a, vector<int> & b)
+    static bool cmp(vector<int>& a, vector<int>& b)
     {
-        return a[1] > b[1];
-    }
-    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-       
-        int cnt=0, ans=0;
-        sort(boxTypes.begin(),boxTypes.end(),comp);
-        
-       for(int i=0;i<boxTypes.size();i++)
-        {
-             cnt = min(truckSize, boxTypes[i][0]);
-             ans += (cnt * boxTypes[i][1]);
-            truckSize -= cnt;
-            if(truckSize == 0)
-                break;
-        }
-        return ans;
+        return b[1] < a[1];
     }
     
+   int maximumUnits(vector<vector<int>>& b, int truckSize) {
+        
+       sort(b.begin(), b.end(), cmp);
+       int sum = 0;
+       int prod = 0;
+       for(int i=0;i<b.size();i++)
+       {
+           sum += b[i][0];
+            if( sum >= truckSize)
+        {
+            sum -= b[i][0];
+            prod += b[i][1] * (truckSize-sum);
+                break;
+        }
+           else{
+               prod +=  b[i][1] * b[i][0];
+       }
+        
+       }      
+            
+       return prod;
+    }
 };
