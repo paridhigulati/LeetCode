6f57e04583079dbcfbab80963653a89aa180ecb4
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-01 12:44:51 +0530

    Time: 562 ms (69.64%), Space: 188.9 MB (5.18%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,78 +1,72 @@
+//need to sort the data in such a way most recently used and least recently used is sorted 
+//cant use vector for that. beech mai se nikal kr aage dalna hoga so using doubly LL 
+
+
+
 class LRUCache {
-public:
-   
-     class node {
-        public:
-            int key;
-            int val;
-            node* next;
-            node* prev;
-        node(int _key, int _val) {
-            key = _key;
-            val = _val; 
-        }
-    };
-    
-    node* head = new node(-1,-1);
-    node* tail = new node(-1,-1);
-    
+    private: 
+    unordered_map<int,int>mp;
+    unordered_map<int, list<int> :: iterator>address; // maps key to corresponding node in LL 
+    list<int>dll;
+    int size; //elements stored till now 
     int cap;
-    unordered_map<int, node*>m;
-    
+public:
     LRUCache(int capacity) {
-        cap = capacity;    
-        head->next = tail;
-        tail->prev = head;
+        cap = capacity;
+        size = 0;
     }
     
-    void addnode(node* newnode) {
-        node* temp = head->next;
-        newnode->next = temp;
-        newnode->prev = head;
-        head->next = newnode;
-        temp->prev = newnode;
-    }
-    
-    void deletenode(node* delnode) {
-        node* delprev = delnode->prev;
-        node* delnext = delnode->next;
-        delprev->next = delnext;
-        delnext->prev = delprev;
-    }
-    
-    int get(int key_) {
-        if (m.find(key_) != m.end()) {
-            node* resnode = m[key_];
-            int res = resnode->val;
-            m.erase(key_);
-            deletenode(resnode);
-            addnode(resnode);
-            m[key_] = head->next;
-            return res; 
-        }
-    
-        return -1;
-    }
-    
-    void put(int key_, int value) {
-        if(m.find(key_) != m.end()) {
-            node* existingnode = m[key_];
-            m.erase(key_);
-            deletenode(existingnode);
-        }
-        if(m.size() == cap) {
-          m.erase(tail->prev->key);
-          deletenode(tail->prev);
-        }
+    int get(int key) {
         
-        addnode(new node(key_, value));
-        m[key_] = head->next; 
+         if(mp.find(key) == mp.end())    
+        return -1;
+                //to make it most recently : need to get the node and for that we make another map
+        
+       list<int> :: iterator it = address[key]; //node ka address pta chlgya jaha delete krna h
+        
+        dll.erase(it);
+        address.erase(key); //map mai se bhi delete
+        dll.push_front(key); //list ke head mai insert
+        address[key] = dll.begin(); //map mai list ka address update 
+        return mp[key];
+        
+       
+    }
+    
+    void put(int key, int value) {
+        
+        //key already exist, not key-value pair so purani wali delete krdo aur new updae
+        if(mp.find(key) != mp.end())
+        {
+            dll.erase(address[key]);
+              address.erase(key);
+               mp.erase(key);
+            size--;
+        }
+        if(size == cap)
+        {
+        int keyy =  dll.back();
+        dll.pop_back();
+        address.erase(keyy);
+        mp.erase(keyy);
+        size--;
+    }
+        
+        size++;
+        dll.push_front(key);
+        address[key] = dll.begin();
+         mp[key] = value;
+        
+        
+        
+        
+        
     }
 };
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
-
