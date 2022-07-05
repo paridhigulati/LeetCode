b94c2227d04ee90ec01e6f9f9d83fc3cc5310d2a
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-05 14:21:29 +0530

    Time: 2 ms (51.88%), Space: 6.4 MB (50.92%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     
-    bool check(char open, char close)
+    
+    
+    bool isMatch(char open , char close)
     {
-        if(open == '(' and close == ')' || open == '{' and close == '}' || open == '[' and close == ']' )
+        if(open == '(' and close == ')' || open=='{' and close == '}' || open == '[' and close == ']')
             return true;
-        else 
-            return false;
+        
+        return false;
     }
     
+    
+    
     bool isValid(string s) {
-       
+        
         stack<char>st;
         
         for(int i=0;i<s.size();i++)
@@ -18,19 +22,21 @@ public:
             if(s[i] == '(' || s[i] == '{' || s[i] == '[')
                 st.push(s[i]);
             
-            else {
-                
-                if(!st.empty() and check(st.top(), s[i]))
-                    st.pop();
-                else
-                    return false;
+            else
+            {
+                if(st.size()>0 and isMatch(st.top(), s[i]))
+            {
+                st.pop();
                 
             }
+                else
+                {
+                    return false;
+                }
         }
-            if(st.size()  == 0)
-                return true;
-            
-            return false;
+        }
+            if(st.size() == 0) return true;
         
+     return false;   
     }
 };
