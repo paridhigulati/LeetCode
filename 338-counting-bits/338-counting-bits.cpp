5293e4af082c28c05055e4c3f201dd2a0f720ce9
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-09 11:09:26 +0530

    Time: 7 ms (71.16%), Space: 8.5 MB (21.13%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
+    
+    int bits(int i)
+    {
+        int cnt=0;
+        
+           while(i > 0)
+        {
+            cnt+= i&1;
+            
+           i = i >> 1;
+        }
+        return cnt;
+        }
+    
     vector<int> countBits(int n) {
-        // 1.recursive : observation---> even : x/2 odd : x/2 + 1 
-        // memoise it to optimise 
-        vector<int>ans(n+1);
-         
+     
+        vector<int>ans;
+        int cnt=0;
+        
         for(int i=0;i<=n;i++)
         {
-            ans[i] = rec(i, ans);
+            int bit = bits(i);
+            ans.push_back(bit);
         }
+     
         return ans;
-        
-    }
-     int rec(int num, vector<int>& ans)
-    {
-        if(num==0) return 0;
-         if(num==1) return 1;
-         if(ans[num]!=0) 
-          return ans[num];
-         if(num%2==0) 
-         {
-             ans[num] = rec(num/2, ans);
-             return rec(num/2, ans);
-         }
-         else
-         {
-             ans[num] = 1+rec(num/2, ans);
-             return 1 + rec(num/2, ans);
-         }
-        
     }
 };
+  
+  
+
+
+
+
+  
