59edf3dffad61deff8b79b8d2dc4af74a79343e8
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-09 13:24:41 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.06%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     
- int dp[1001];
+//  int dp[1001];
     
-    bool solve(int n)
-    {
-        if(n==1) return 0;
+//     bool solve(int n)
+//     {
+//         if(n==1) return 0;
         
-        if(dp[n]!= -1)
-         return dp[n];
+//         if(dp[n]!= -1)
+//          return dp[n];
         
-        else
-        {
-        for(int i=1;i<(sqrt(n));i++)
-        {
-            if(n%i == 0 )
-            {
-                if( solve(n-i) ==0)
-                    return dp[n]=1;
-            }
-        }
+//         else
+//         {
+//         for(int i=1;i<(sqrt(n));i++)
+//         {
+//             if(n%i == 0 )
+//             {
+//                 if( solve(n-i) ==0)
+//                     return dp[n]=1;
+//             }
+//         }
         
-        return dp[n] = 0;
-        }
-    }
+//         return dp[n] = 0;
+//         }
+//     }
 
 
     
     bool divisorGame(int n) {
-        memset(dp,-1,sizeof dp);
-        return solve(n);
+        // memset(dp,-1,sizeof dp);
+        // return solve(n);
+        return n%2 == 0;
     }
     
 };
