838523e1aea6a0f9195d01cb3fefd292bf69f326
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-10 10:45:03 +0530

    Time: 107 ms (5.21%), Space: 32.7 MB (42.75%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -15,11 +15,43 @@ public:
      
         if(!root1) return root2;
         if(!root2) return root1;
-        root1->val += root2->val;
+        stack<TreeNode*>st, stt;
+        st.push(root1);
+        stt.push(root2);
         
-        root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
+        while(!st.empty())
+        {
+            auto t1 = st.top();
+            st.pop();
+            auto t2 = stt.top();
+            stt.pop();
+            
+            t1->val += t2->val;
+            
+            if(!t1->left and t2->left)
+                t1->left = t2->left;
+            
+            else if(t1->left and t2->left)
+            {
+                st.push(t1->left);
+                stt.push(t2->left);
+                }
+                  
+            if(!t1->right and t2->right)
+                t1->right = t2->right;
+            
+            else if(t1->right and t2->right)
+            {
+                st.push(t1->right);
+                stt.push(t2->right);
+                }
+        }
+return root1;
+        }
+    };
+
         
-        return root1;
-    }
-};
+        
+        
+      
+         
