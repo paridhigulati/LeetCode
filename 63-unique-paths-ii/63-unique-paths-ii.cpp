acf8e065b872a34c81dbf7f0676061bfe72a50b6
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-13 12:44:55 +0530

    Time: 9 ms (20.42%), Space: 7.9 MB (12.57%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,30 +3,23 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& a) {
   
         int  m = a.size(), n=a[0].size();
-          vector<vector<int>>dp(m, vector<int>(n,-1));
-        return solve(0,0,a, dp);
-        
-    }
-    
-        int solve(int i , int j, vector<vector<int>>& a , vector<vector<int>> & dp)
+          vector<vector<int>>dp(m+1, vector<int>(n+1));
+           
+        dp[0][1] =  1;
+        for(int  i=1;i<=m;i++)
         {
-              int  m = a.size(), n=a[0].size();
-          
-            
-        
-        if(i==m-1 and j== n-1 and !a[i][j]) return  1;
-        
-        if(j>=n || j < 0 || i< 0|| i>=m) return 0;
-            
-            if(dp[i][j] != -1) return dp[i][j];
-        
-            if(a[i][j]) return 0;
-            
-        
-       return   dp[i][j] =  solve(i+1, j, a,dp) + solve(i, j+1, a,dp);
+            for(int j=1;j<=n;j++)
+            {
+               if(a[i-1][j-1]==1)
+               {
+                   dp[i][j] = 0;
+               }
+                else
+                   
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m][n];
         
     }
 };
-    
-    
-   
