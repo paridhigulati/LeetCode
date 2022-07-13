3e11c26e37999f0ee087fefabd16bd8a0322d054
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-13 12:14:14 +0530

    Time: 7 ms (36.29%), Space: 7.8 MB (42.94%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+    int uniquePathsWithObstacles(vector<vector<int>>& a) {
+  
+        int  m = a.size(), n=a[0].size();
+          vector<vector<int>>dp(m, vector<int>(n,-1));
+        return solve(0,0,a, dp);
         
-        int m = grid.size(), n = grid[0].size();
-       
-       // vector<int>dp(m, vector<int>(n));
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+    }
     
-        for(int i=0;i<m;i++)
+        int solve(int i , int j, vector<vector<int>>& a , vector<vector<int>> & dp)
         {
+              int  m = a.size(), n=a[0].size();
           
-        for(int j=0;j<n;j++)
-        {
-          if(grid[i][j] == 1) dp[i][j] =0;
-            else if(i==0 and j==0)
-                dp[i][j] = 1;
-            else
-            {
-                 int up=0, down = 0;
-             if(i>0)
-                  up = dp[i-1][j];
-            if (j>0)  
-               down = dp[i][j-1];
-            dp[i][j] = up + down;
-            }
-        }
-        }
-            return dp[m-1][n-1];
-        }
-         
+            
+        
+        if(i==m-1 and j== n-1 and !a[i][j]) return  1;
+        
+        if(j>=n || j < 0 || i< 0|| i>=m) return 0;
+            
+            if(dp[i][j] != -1) return dp[i][j];
+        
+            if(a[i][j]) return 0;
+            
+        
+       return   dp[i][j] =  solve(i+1, j, a,dp) + solve(i, j+1, a,dp);
+        
+    }
 };
+    
+    
+   
