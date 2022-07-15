a37de6dc9b022122d5fd78feb90e1e02bd917579
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-15 13:56:09 +0530

    Time: 7 ms (57.73%), Space: 10.6 MB (96.38%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,33 +11,25 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        /*brute: count the nodes O(n)
-	ListNode* iter = head;
-	int len = 0, i = 1;
-	while(iter)
-    iter = iter -> next, len++;    // finding the length of linked list
-	if(len == n) 
+  
         
-    return head -> next;          // if head itself is to be deleted, just return head -> next
-	for(iter = head; i < len - n; i++)
-      iter = iter -> next; // iterate first len-n nodes
-	iter -> next = iter -> next -> next;      // remove the nth node from the end
-	return head;
+        
+    ListNode* start = head, *end=head;
+        
+        while(n--)
+        {
+            end = end->next;
+        }
+        if(!end) return head->next;
+        
+        
+        while(end->next != NULL )
+        {
+            end = end->next;
+            start = start->next;
+        }
+        start->next = start->next->next;
+        
+        return head;
     }
-};*/
-        //OPTIMAL 1 PASS -> SLOW AND FAST POINTERS 
-        
-        //Now, between the fast and slow pointers, there is a gap of n nodes. Now, just Iterate and increment both the pointers till fast reaches the last node. The gap between fast and slow is still of n nodes, meaning that slow is nth node from the last node (which currently is fast).
-        
-	ListNode *fast = head, *slow = head;
-	while(n--)
-    fast = fast -> next;      // iterate first n nodes using fast
-	if(!fast)
-    return head -> next;       // if fast is already null, it means we have to delete head itself. So, just return next of head
-	while(fast -> next)                  // iterate till fast reaches the last node of list
-	fast = fast -> next, slow = slow -> next;            
-	 slow -> next = slow -> next -> next; // remove the nth node from last
-        //delete slow->next;
-	return head;
-}
 };
