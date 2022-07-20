c1cf82b8ef2b41f399303773b755f6affe5c4b56
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-20 17:28:59 +0530

    Time: 7 ms (85.56%), Space: 13.1 MB (47.28%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,59 +1,42 @@
-class Solution {
+
+     class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-  
-        int row = grid.size(), col = grid[0].size();
         
-        int fresh=0, time=-1;
+        int m = grid.size();
+        int n = grid[0].size();
         
-        queue<pair<int,int>>q;
-                vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
-
-        //vector<vector<int>>dir = { {0,1}, {1,0}, {-1,0}, {0,-1}};
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(grid[i][j]==2)
-                {
-                    q.push({i,j});
-                }
-                 if(grid[i][j] == 1)
-                {
-                    fresh++;
-                }
+        int fresh = 0, time = -1;
+        
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        queue<pair<int, int>> q;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2) q.push({i, j});
             }
         }
         
-        while(!q.empty())
-              {
-            int size = q.size();
-                  for(int i=0;i<size;i++)
-                  {
-                      auto  front = q.front();
-                      q.pop();
-                      
-                      for(int i=0;i<4;i++)
-                      {
-                          int x  = front.first + dir[i];
-                          int y = front.second + dir[i+1];
-                          
-                          if(x >=0 and x <row and y>=0 and y<col and grid[x][y]==1)
-                          {
-                              fresh--;
-                              grid[x][y] = 2;
-                              q.push({x,y});
-                          }
-                      }
-                  }
-        
-              time++;
+        while(!q.empty()) {
+            int sz = q.size();
+            for(int i = 0; i < sz; i++) {
+                auto cur = q.front();
+                q.pop();
+                for(auto dir: dirs) {
+                    int x = cur.first + dir.first;
+                    int y = cur.second + dir.second;
+                    if(x >= 0 and x < m and y >= 0 and y < n and grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        q.push({x, y});
+                        fresh--;
+                    }
+                }
+            }
+            time++;
         }
-    
-        if(fresh > 0) return -1;
         
-        if(time ==-1) return 0;
-        
-        return time;
+        return (fresh > 0) ? -1 : ((time == -1) ? 0 : time);
     }
 };
+
