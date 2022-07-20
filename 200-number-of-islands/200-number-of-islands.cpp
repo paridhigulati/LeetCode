a15250547ba187b6f74f6b06f7b1c76d0b41eeb2
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-20 16:53:49 +0530

    Time: 238 ms (5.03%), Space: 34.6 MB (5.08%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,17 +3,14 @@ public:
     int numIslands(vector<vector<char>>& grid) {
      
         int n=grid.size(), m=grid[0].size();
-        
         int count=0;
-        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j] == '1')
                 {
-                    
-                    bfs(i,j,grid);
+                    bfs(grid, i, j);
                     count++;
                 }
             }
@@ -21,39 +18,32 @@ public:
         return count;
     }
     
+    
+    void bfs(vector<vector<char>>& grid, int i, int j)
+    {
         
-        void bfs(int i, int j, vector<vector<char>>& grid){
-                 
-         int n = grid.size();
-        int m = grid[0].size();
-                    vector<vector<int>>dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};
-
-        
-         queue<pair<int,int>>q;
-            
+        int n=grid.size(), m=grid[0].size();
+        queue<pair<int,int>>q;
         q.push({i,j});
-            grid[i][j] = '0';
-            
+        grid[i][j]='0';
+        vector<vector<int>>dir = { {0,1}, {1,0}, {0,-1}, {-1,0}};
         while(!q.empty())
         {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
             
-                auto it = q.front();
-                q.pop();
+            for(auto i : dir)
+            {
+                int nrow = row + i[0];
+                int ncol = col + i[1];
                 
-                for(auto itr : dir)
+                if(nrow>=0 and nrow < n and ncol >=0 and ncol <m and grid[nrow][ncol]=='1' )
                 {
-                   
-                    int x = it.first + itr[0];
-                    int y = it.second + itr[1];
-                    
-                    if(x>=0 and x<n and y>=0 and y<m and grid[x][y] == '1')
-                    {
-                        grid[x][y]='0';
-                        q.push({x,y});
-                    }
+                    q.push({nrow, ncol});
+                    grid[nrow][ncol]='0';
                 }
-            
+            }
         }
-        
     }
 };
