8c1c4ac68672cc03390102cc9a1bf2a9642bb38b
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2022-07-27 08:46:13 +0530

    Time: 57 ms (71.90%), Space: 14.7 MB (31.35%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,26 +9,22 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        /*
-        Infact, we don't need to find diff and iterate the greater length list. Instead, if we reach the end of a list, just restart from the head of other list. This can help up counter the difference of lengths. Finally, the lists will either converge on intersected node or both will reach end at same time and we will return NULL at last.
-
-For eg. Lists having intersected node (Example 1 from description) -
-
-A : 4 -> 1 -> 8 -> 4 -> 5 |  5 -> 6 -> 1 -> 8 <- same node found
-						  ^end(restart from B)
-B : 5 -> 6 -> 1 -> 8 -> 4 -> 5 |  4 -> 1 -> 8 <- same node found
-							   ^end(restart from A)
-return since we found the same node*/
-        if(headA==NULL ||headB ==NULL)
         
-            return NULL;  
-        ListNode* a  = headA;
-        ListNode* b = headB;
+        ListNode* a = headA, *b = headB;
+           
         while(a!=b)
         {
-          a = a==NULL?headB:a->next;
-            b= b==NULL?headA: b->next;
+            if(a==NULL) 
+                a = headB;
+            else
+                a = a->next;
+            
+            if(b==NULL) 
+                b = headA;
+            else
+                b = b->next;
+            
         }
-        return b;
+        return a;
     }
 };
